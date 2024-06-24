7ff823dbe7ae85052b4e84f23d4e87c900d8c171
Ricki Jay <ricki.jay@wikimedia.de>
2024-06-25 01:08:20 +0200

    Black

--- model/strawberry/output/observation/property_popularity/property_popularity_count.py
+++ model/strawberry/output/observation/property_popularity/property_popularity_count.py
@@ -6,7 +6,6 @@ import strawberry
 from model.database import WikibasePropertyPopularityCountModel
 
 
-
 # This is needed because GraphQL does not support 64 bit integers
 BigInt = strawberry.scalar(
     Union[int, str],  # type: ignore
@@ -14,15 +13,16 @@ BigInt = strawberry.scalar(
     parse_value=lambda v: str(v),
     description="BigInt field",
 )
- 
+
+
 @strawberry.type
 class WikibasePropertyPopularityCountStrawberryModel:
     """Wikibase Property Popularity Count"""
 
     id: strawberry.ID
     property_url: str = strawberry.field(description="Property URL")
-    usage_count = strawberry.field(graphql_type=BigInt,
-        description="Number of Triples with this Property"
+    usage_count = strawberry.field(
+        graphql_type=BigInt, description="Number of Triples with this Property"
     )
 
     @classmethod
