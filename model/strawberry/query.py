efdeb438c68ca47edc6241e22b79ecd246b19620
Ricki Jay <ricki.jay@wikimedia.de>
2024-06-26 00:11:56 +0200

    Refactor

--- model/strawberry/query.py
+++ model/strawberry/query.py
@@ -5,9 +5,11 @@ import strawberry
 
 from model.database import WikibaseSoftwareTypes
 from model.strawberry.output import (
+    Page,
+    WikibasePropertyPopularityAggregateCountStrawberryModel,
     WikibaseSoftwareVersionDoubleAggregateStrawberryModel,
+    WikibaseStrawberryModel,
 )
-from model.strawberry.output.page import Page
 from resolvers import (
     get_aggregate_property_popularity,
     get_aggregate_version,
@@ -21,14 +23,15 @@ class Query:
     """GraphQL Query"""
 
     wikibase = strawberry.field(description="Wikibase Instance", resolver=get_wikibase)
-    wikibase_list = strawberry.field(
-        description="List of Wikibases", resolver=get_wikibase_list
-    )
 
-    aggregate_property_popularity = strawberry.field(
-        description="Aggregated Property Popularity - from the most current property popularity for each wikibase, ordered by # wikibases desc, # uses desc",
-        resolver=get_aggregate_property_popularity,
-    )
+    @strawberry.field(description="List of Wikibases")
+    async def wikibase_list(
+        page_number: Annotated[
+            int, strawberry.argument(description="Page Number - 1-indexed")
+        ],
+        page_size: Annotated[int, strawberry.argument(description="Page Size")],
+    ) -> Page[WikibaseStrawberryModel]:
+        return await get_wikibase_list(page_number, page_size)
 
     @strawberry.field(description="Aggregated Extension Popularity")
     async def aggregate_extension_popularity(
@@ -51,6 +54,15 @@ class Query:
         return await get_aggregate_version(
             WikibaseSoftwareTypes.library, page_number, page_size
         )
+
+    @strawberry.field(description="Aggregated Property Popularity")
+    async def aggregate_property_popularity(
+        page_number: Annotated[
+            int, strawberry.argument(description="Page Number - 1-indexed")
+        ],
+        page_size: Annotated[int, strawberry.argument(description="Page Size")],
+    ) -> Page[WikibasePropertyPopularityAggregateCountStrawberryModel]:
+        return await get_aggregate_property_popularity(page_number, page_size)
 
     @strawberry.field(description="Aggregated Skin Popularity")
     async def aggregate_skin_popularity(
