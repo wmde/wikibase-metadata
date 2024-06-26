8b68e869e473b1fe483246248c9866e1baaa2e91
Ricki Jay <ricki.jay@wikimedia.de>
2024-06-26 21:04:33 +0200

    Unused

--- src/utils/assign_cell.py
+++
@@ -1,6 +0,0 @@
-def assign_cell(
-    worksheet, x: int, y: int, value: bool | int | float | str, overwrite: bool = False
-) -> None:
-    c = worksheet.cell(y, x)
-    if c.value is None or overwrite:
-        c.value = value
