bee5e167c676f9b621c82ca8a6bf8f97137f5130
Ricki <141253288+RickiJay-WMDE@users.noreply.github.com>
2024-06-26 18:02:25 +0200

    BigInt (#5)

---
+++ model/strawberry/scalars/big_int.py
@@ -0,0 +1,13 @@
+"""Big Int"""
+
+from typing import Union
+import strawberry
+
+
+# This is needed because GraphQL does not support 64 bit integers
+BigInt = strawberry.scalar(
+    Union[int, str],  # type: ignore
+    serialize=lambda v: int(v),
+    parse_value=lambda v: str(v),
+    description="BigInt field",
+)
