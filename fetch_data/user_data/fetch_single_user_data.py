4c45e6ea0634bb67a4f8076d6d82ebce23a54964
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-03 23:16:48 +0200

    Limit

--- fetch_data/user_data/fetch_single_user_data.py
+++ fetch_data/user_data/fetch_single_user_data.py
@@ -18,8 +18,19 @@ def get_single_user_data(wikibase: WikibaseModel, user: str) -> dict:
 def get_multiple_user_data(wikibase: WikibaseModel, users: Iterable[str]) -> list[dict]:
     """Get User Data"""
 
+    QUERY_LIMIT = 50
+
     if len(users) == 0:
         return []
+
+    if len(users) > QUERY_LIMIT:
+        data = []
+        list_users = list(users)
+        for i in range(0, len(users), QUERY_LIMIT):
+            data.extend(
+                get_multiple_user_data(wikibase, list_users[i : i + QUERY_LIMIT])
+            )
+        return data
 
     result = requests.get(
         wikibase.action_api_url.url + user_url("|".join(users)), timeout=10
