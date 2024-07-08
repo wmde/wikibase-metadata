b5005ce12f0e06541bab82da29c6db1226fdffc7
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-08 14:37:10 +0200

    Total Admin

--- model/strawberry/output/observation/user/wikibase_user_aggregate.py
+++ model/strawberry/output/observation/user/wikibase_user_aggregate.py
@@ -2,8 +2,19 @@
 
 import strawberry
 
+from model.strawberry.scalars.big_int import BigInt
+
 
 @strawberry.type
 class WikibaseUserAggregate:
-    total_users: int
-    wikibase_count: int
+    """User Aggregate"""
+
+    total_admin: int = strawberry.field(
+        description="Total Administrators (estimated from group names)",
+        graphql_type=BigInt,
+    )
+    total_users: int = strawberry.field(
+        description="Total Users in all Wikibases (DOES NOT ACCOUNT FOR OVERLAP)",
+        graphql_type=BigInt,
+    )
+    wikibase_count: int = strawberry.field(description="Wikibases with User Data")
--- resolvers/get_aggregate_users.py
+++ resolvers/get_aggregate_users.py
@@ -2,9 +2,15 @@
 
 from typing import Tuple
 
-from sqlalchemy import Select, select, func
+from sqlalchemy import Select, and_, or_, select, func
 
 from data import get_async_session
+from model.database.wikibase_observation.user.wikibase_user_group_model import (
+    WikibaseUserGroupModel,
+)
+from model.database.wikibase_observation.user.wikibase_user_observation_group_model import (
+    WikibaseUserObservationGroupModel,
+)
 from model.database.wikibase_observation.user.wikibase_user_observation_model import (
     WikibaseUserObservationModel,
 )
@@ -16,18 +22,64 @@ from model.strawberry.output.observation.user.wikibase_user_aggregate import (
 async def get_aggregate_users() -> WikibaseUserAggregate:
     """Get Aggregate Property Popularity"""
 
-    query = get_aggregate_query()
+    total_user_query = get_total_user_query()
+    total_admin_query = get_total_admin_query()
 
     async with get_async_session() as async_session:
-        total_users, wikibase_count = (await async_session.execute(query)).one()
+        total_users, wikibase_count = (
+            await async_session.execute(total_user_query)
+        ).one()
+        (total_admin,) = (await async_session.execute(total_admin_query)).one()
 
         return WikibaseUserAggregate(
-            total_users=total_users, wikibase_count=wikibase_count
+            total_admin=total_admin,
+            total_users=total_users,
+            wikibase_count=wikibase_count,
         )
 
 
-def get_aggregate_query() -> Select[Tuple[int, int]]:
-    """Get Unordered Property Popularity Query"""
+def get_total_admin_query() -> Select[Tuple[int]]:
+    """Get Total Admin Query"""
+
+    rank_subquery = (
+        select(
+            WikibaseUserObservationModel.id,
+            func.rank()  # pylint: disable=not-callable
+            .over(
+                partition_by=WikibaseUserObservationModel.wikibase_id,
+                order_by=WikibaseUserObservationModel.observation_date.desc(),
+            )
+            .label("rank"),
+        )
+        .where(WikibaseUserObservationModel.returned_data)
+        .subquery()
+    )
+    query = (
+        select(
+            func.sum(WikibaseUserObservationGroupModel.user_count).label("total_admins")
+        )
+        .join(
+            rank_subquery,
+            onclause=WikibaseUserObservationGroupModel.wikibase_user_observation_id
+            == rank_subquery.c.id,
+        )
+        .where(
+            and_(
+                rank_subquery.c.rank == 1,
+                WikibaseUserObservationGroupModel.user_group.has(
+                    or_(
+                        WikibaseUserGroupModel.group_name.in_(["bureaucrat", "sysop"]),
+                        WikibaseUserGroupModel.group_name.like(r"%admin%"),
+                    )
+                ),
+            )
+        )
+    )
+    return query
+
+
+def get_total_user_query() -> Select[Tuple[int, int]]:
+    """Get Total User Query"""
 
     rank_subquery = (
         select(
