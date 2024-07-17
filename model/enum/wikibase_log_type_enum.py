3428fd4380c8158663be3bfc18aecdf1947e22de
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-17 11:47:41 +0200

    uPDATES

--- model/enum/wikibase_log_type_enum.py
+++ model/enum/wikibase_log_type_enum.py
@@ -8,6 +8,7 @@ class WikibaseLogType(enum.Enum):
     """Wikibase Log Type"""
 
     ABUSE_FILTER_CREATE = 1
+    ABUSE_FILTER_MODIFIY = 43
     COMMENTS_CREATE = 2
     COMMENTS_DELETE = 3
     CONSUMER_APPROVE = 4
@@ -16,26 +17,28 @@ class WikibaseLogType(enum.Enum):
     CONSUMER_REJECT = 7
     CONSUMER_UPDATE = 39
     EVENT_DELETE = 37
-    IMAGE_OVERWRITE = 8
-    IMAGE_REVERT = 9
-    IMAGE_UPLOAD = 10
     IMPORT = 11
     INTERWIKI_CREATE = 12
     INTERWIKI_DELETE = 13
     INTERWIKI_EDIT = 14
     ITEM_CREATE = 15
     ITEM_DELETE = 16
+    MEDIA_OVERWRITE = 8
+    MEDIA_REVERT = 9
+    MEDIA_UPLOAD = 10
     MOVE = 17
     PAGE_CREATE = 18
     PAGE_DELETE = 19
     PATROL = 20
     PATROL_AUTO = 21
+    PROFILE = 42
     PROPERTY_CREATE = 22
     PROPERTY_DELETE = 23
     PROTECT = 24
     REVISION_DELETE = 38
     TAG_CREATE = 25
     UNDO_DELETE = 26
+    UNPROTECT = 41
     USER_BLOCK = 27
     USER_UNBLOCK = 28
     USER_CREATE = 29
@@ -43,9 +46,15 @@ class WikibaseLogType(enum.Enum):
     USER_MERGE = 31
     USER_RENAME = 32
     USER_RIGHTS = 33
+    WIKI_FARM = 40
     WIKI_NAMESPACES = 34
     WIKI_RIGHTS = 35
     WIKI_SETTINGS = 36
+
+
+MEDIA_REGEX = re.compile(r".*\.(flv|jpg|png)", re.IGNORECASE)
+ITEM_REGEX = re.compile(r"Item:Q\d+")
+PROPERTY_REGEX = re.compile(r"(Property|WikibaseProperty):P\d+")
 
 
 # pylint: disable=too-many-statements
@@ -55,24 +64,23 @@ def compile_log_type(record: dict) -> WikibaseLogType:
     log_type: WikibaseLogType
     match (record["type"], record["action"]):
         case ("create", "create"):
-            if re.match(r"Item:Q\d+", record["title"]) is not None:
+            if ITEM_REGEX.match(record["title"]) is not None:
                 log_type = WikibaseLogType.ITEM_CREATE
-            elif re.match(r"WikibaseProperty:P\d+", record["title"]) is not None:
+            elif PROPERTY_REGEX.match(record["title"]) is not None:
                 log_type = WikibaseLogType.PROPERTY_CREATE
             else:
                 log_type = WikibaseLogType.PAGE_CREATE
         case ("delete", "delete"):
-            if re.match(r"Item:Q\d+", record["title"]):
+            if ITEM_REGEX.match(record["title"]):
                 log_type = WikibaseLogType.ITEM_DELETE
-            elif (
-                re.match(r"WikibaseProperty:P\d+", record["title"]) is not None
-                or re.match(r"Property:P\d+", record["title"]) is not None
-            ):
+            elif PROPERTY_REGEX.match(record["title"]) is not None:
                 log_type = WikibaseLogType.PROPERTY_DELETE
             else:
                 log_type = WikibaseLogType.PAGE_DELETE
         case ("abusefilter", "create"):
             log_type = WikibaseLogType.ABUSE_FILTER_CREATE
+        case ("abusefilter", "modify"):
+            log_type = WikibaseLogType.ABUSE_FILTER_MODIFIY
         case ("comments", "add"):
             log_type = WikibaseLogType.COMMENTS_CREATE
         case ("comments", "delete"):
@@ -89,21 +97,6 @@ def compile_log_type(record: dict) -> WikibaseLogType:
             log_type = WikibaseLogType.CONSUMER_UPDATE
         case ("delete", "event"):
             log_type = WikibaseLogType.EVENT_DELETE
-        case ("upload", "overwrite"):
-            if "img_sha1" in record["params"]:
-                log_type = WikibaseLogType.IMAGE_OVERWRITE
-            else:
-                raise NotImplementedError(record)
-        case ("upload", "revert"):
-            if "img_sha1" in record["params"]:
-                log_type = WikibaseLogType.IMAGE_REVERT
-            else:
-                raise NotImplementedError(record)
-        case ("upload", "upload"):
-            if "img_sha1" in record["params"]:
-                log_type = WikibaseLogType.IMAGE_UPLOAD
-            else:
-                raise NotImplementedError(record)
         case ("import", "upload"):
             log_type = WikibaseLogType.IMPORT
         case ("interwiki", "iw_add"):
@@ -112,12 +105,29 @@ def compile_log_type(record: dict) -> WikibaseLogType:
             log_type = WikibaseLogType.INTERWIKI_DELETE
         case ("interwiki", "iw_edit"):
             log_type = WikibaseLogType.INTERWIKI_EDIT
+        case ("upload", "overwrite"):
+            if "img_sha1" in record["params"] or MEDIA_REGEX.match(record["title"]):
+                log_type = WikibaseLogType.MEDIA_OVERWRITE
+            else:
+                raise NotImplementedError(record)
+        case ("upload", "revert"):
+            if "img_sha1" in record["params"] or MEDIA_REGEX.match(record["title"]):
+                log_type = WikibaseLogType.MEDIA_REVERT
+            else:
+                raise NotImplementedError(record)
+        case ("upload", "upload"):
+            if "img_sha1" in record["params"] or MEDIA_REGEX.match(record["title"]):
+                log_type = WikibaseLogType.MEDIA_UPLOAD
+            else:
+                raise NotImplementedError(record)
         case ("move", "move"):
             log_type = WikibaseLogType.MOVE
         case ("patrol", "autopatrol"):
             log_type = WikibaseLogType.PATROL_AUTO
         case ("patrol", "patrol"):
             log_type = WikibaseLogType.PATROL
+        case ("profile", "profile"):
+            log_type = WikibaseLogType.PROFILE
         case ("protect", "move_prot") | ("protect", "protect") | ("protect", "modify"):
             log_type = WikibaseLogType.PROTECT
         case ("delete", "revision"):
@@ -126,6 +136,8 @@ def compile_log_type(record: dict) -> WikibaseLogType:
             log_type = WikibaseLogType.TAG_CREATE
         case ("delete", "restore"):
             log_type = WikibaseLogType.UNDO_DELETE
+        case ("protect", "unprotect"):
+            log_type = WikibaseLogType.UNPROTECT
         case ("block", "block") | ("block", "reblock"):
             log_type = WikibaseLogType.USER_BLOCK
         case ("block", "unblock"):
@@ -141,10 +153,12 @@ def compile_log_type(record: dict) -> WikibaseLogType:
             log_type = WikibaseLogType.USER_DELETE
         case ("usermerge", "mergeuser"):
             log_type = WikibaseLogType.USER_MERGE
-        case ("rights", "rights"):
+        case ("rights", "rights") | ("rights", "blockautopromote"):
             log_type = WikibaseLogType.USER_RIGHTS
         case ("renameuser", "renameuser"):
             log_type = WikibaseLogType.USER_RENAME
+        case ("farmer", "managewiki"):
+            log_type = WikibaseLogType.WIKI_FARM
         case ("managewiki", "namespaces"):
             log_type = WikibaseLogType.WIKI_NAMESPACES
         case ("managewiki", "rights"):
