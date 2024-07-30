6f15c3975561526b574f5cfaaf750e30cd6e0eec
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-30 12:58:04 +0200

    test_get_log_param_string

---
+++ tests/test_create_log_observation.py
@@ -0,0 +1,20 @@
+from fetch_data.log_data.fetch_log_data import get_log_param_string
+
+
+def test_get_log_url():
+    assert (
+        get_log_param_string()
+        == "?action=query&format=json&list=logevents&formatversion=2&ledir=older"
+    )
+    assert (
+        get_log_param_string(limit=100)
+        == "?action=query&format=json&list=logevents&formatversion=2&ledir=older&lelimit=100"
+    )
+    assert (
+        get_log_param_string(oldest=True)
+        == "?action=query&format=json&list=logevents&formatversion=2&ledir=newer"
+    )
+    assert (
+        get_log_param_string(offset="20200101000000|1")
+        == "?action=query&format=json&list=logevents&formatversion=2&ledir=older&lecontinue=20200101000000|1"
+    )
