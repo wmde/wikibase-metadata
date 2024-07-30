6073844a53a396d611fdeac98d9382fc316e769a
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-30 17:18:54 +0200

    Error

--- tests/test_create_log_observation/test_create_log_observation.py
+++ tests/test_create_log_observation/test_create_log_observation.py
@@ -4,13 +4,14 @@ from datetime import datetime, timedelta
 
 from freezegun import freeze_time
 import pytest
+from requests import ReadTimeout
 from fetch_data.log_data import create_log_observation
 
 
 @freeze_time("2024-03-01")
 @pytest.mark.asyncio
 async def test_create_log_observation(mocker):
-    """Test One-Pull, Newest Logs Scenario"""
+    """Test One-Pull Per Month, Data Returned Scenario"""
 
     mock_logs: list[dict] = []
     for i in range(70):
@@ -18,6 +19,62 @@ async def test_create_log_observation(mocker):
             {
                 "logid": i + 1,
                 "timestamp": (datetime(2024, 3, 1) - timedelta(hours=i * 24)).strftime(
+                    "%Y-%m-%dT%H:%M:%SZ"
+                ),
+                "user": None,
+                "type": "thanks",
+                "action": "thank",
+            }
+        )
+    oldest_mock_log = min(mock_logs, key=lambda x: x.get("timestamp"))
+    newest_mock_log = max(mock_logs, key=lambda x: x.get("timestamp"))
+
+    mocker.patch(
+        "fetch_data.log_data.fetch_log_data.fetch_api_data",
+        side_effect=[
+            {"query": {"logevents": [oldest_mock_log]}},  # oldest
+            {"query": {"logevents": [newest_mock_log]}},  # newest
+            {
+                "query": {
+                    "logevents": sorted(mock_logs, key=lambda x: x.get("timestamp"))
+                },
+            },  # first month
+            {
+                "query": {
+                    "logevents": sorted(
+                        mock_logs, key=lambda x: x.get("timestamp"), reverse=True
+                    )
+                },
+            },  # last month
+        ],
+    )
+    results = await create_log_observation(1)
+    assert results
+
+
+@freeze_time("2024-03-01")
+@pytest.mark.asyncio
+async def test_create_log_observation_error(mocker):
+    """Test One-Pull Per Month, Error Returned Scenario"""
+
+    mocker.patch(
+        "fetch_data.log_data.fetch_log_data.fetch_api_data", side_effect=[ReadTimeout()]
+    )
+    results = await create_log_observation(1)
+    assert results is False
+
+
+@freeze_time("2024-03-01")
+@pytest.mark.asyncio
+async def test_create_log_observation_no_last_month(mocker):
+    """Test One-Pull Per Month, No Data In Range Returned Scenario"""
+
+    mock_logs: list[dict] = []
+    for i in range(70):
+        mock_logs.append(
+            {
+                "logid": i + 1,
+                "timestamp": (datetime(2024, 1, 1) - timedelta(hours=i * 24)).strftime(
                     "%Y-%m-%dT%H:%M:%SZ"
                 ),
                 "user": None,
