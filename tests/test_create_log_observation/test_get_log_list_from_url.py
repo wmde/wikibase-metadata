8f604bc0716b0e84858bc1704bff94b5691dd640
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-30 13:49:35 +0200

    With my freeze ray...

--- tests/test_create_log_observation/test_get_log_list_from_url.py
+++ tests/test_create_log_observation/test_get_log_list_from_url.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+from freezegun import freeze_time
 from fetch_data.log_data.fetch_log_data import get_log_list_from_url
 
 
@@ -10,6 +12,7 @@ def test_get_log_list_from_url_empty(mocker):
     assert get_log_list_from_url("test.url") == []
 
 
+@freeze_time("2024-03-01")
 def test_get_log_list_from_url_single(mocker):
     mocker.patch(
         "fetch_data.log_data.fetch_log_data.fetch_api_data",
@@ -31,6 +34,6 @@ def test_get_log_list_from_url_single(mocker):
     assert len(results) == 1
     assert results[0].id == 1
     assert results[0].log_date == datetime(2024, 1, 1, 12, 4, 15)
-    assert results[0].age() > 0
+    assert results[0].age() == 59
     assert results[0].user is None
     assert results[0].log_type.name == "PROPERTY_CREATE"
