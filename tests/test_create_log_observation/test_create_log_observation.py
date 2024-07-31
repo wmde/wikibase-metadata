65636c8ca2dbcc9d9e5edf18b2be6ea79fff1941
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-31 23:36:17 +0200

    Mock User Data

--- tests/test_create_log_observation/test_create_log_observation.py
+++ tests/test_create_log_observation/test_create_log_observation.py
@@ -1,11 +1,13 @@
 """Test get_month_log_list_from_url"""
 
 from datetime import datetime, timedelta
+import json
 
 from freezegun import freeze_time
 import pytest
 from requests import ReadTimeout
 from fetch_data import create_log_observation
+from tests.utils import MockResponse,ParsedUrl
 
 
 @freeze_time("2024-03-01")
@@ -80,7 +82,7 @@ async def test_create_log_observation_no_last_month(mocker):
                 "timestamp": (datetime(2024, 1, 1) - timedelta(hours=i * 24)).strftime(
                     "%Y-%m-%dT%H:%M:%SZ"
                 ),
-                "user": None,
+                "user": "User:A" if i % 3 == 0 else "User:B" if i % 5 == 0 else None,
                 "type": "thanks",
                 "action": "thank",
             }
@@ -88,24 +90,56 @@ async def test_create_log_observation_no_last_month(mocker):
     oldest_mock_log = min(mock_logs, key=lambda x: x.get("timestamp"))
     newest_mock_log = max(mock_logs, key=lambda x: x.get("timestamp"))
 
+    def mockery(*args, **kwargs):  # pylint: disable=unused-argument
+        query = ParsedUrl(args[0])
+        match (query.params.get("action"), query.params.get("list")):
+            case ("query", "logevents"):
+                match (query.params.get("ledir"), query.params.get("lelimit")):
+                    case ("newer", 1):
+                        return MockResponse(
+                            200, json.dumps({"query": {"logevents": [oldest_mock_log]}})
+                        )
+                    case ("newer", 500):
+                        return MockResponse(
+                            200,
+                            json.dumps(
+                                {
+                                    "query": {
+                                        "logevents": sorted(
+                                            mock_logs, key=lambda x: x.get("timestamp")
+                                        )
+                                    }
+                                }
+                            ),
+                        )
+                    case ("older", 1):
+                        return MockResponse(
+                            200, json.dumps({"query": {"logevents": [newest_mock_log]}})
+                        )
+                    case ("older", 500):
+                        return MockResponse(
+                            200,
+                            json.dumps(
+                                {
+                                    "query": {
+                                        "logevents": sorted(
+                                            mock_logs,
+                                            key=lambda x: x.get("timestamp"),
+                                            reverse=True,
+                                        )
+                                    }
+                                }
+                            ),
+                        )
+            case ("query", "users"):
+                users = []
+                if "User:A" in query.params.get("ususers"):
+                    users.append({"name": "User:A", "groups": ["*", "users", "admin"]})
+                return MockResponse(200, json.dumps({"query": {"users": users}}))
+        raise NotImplementedError(query)
+
     mocker.patch(
-        "fetch_data.log_data.fetch_log_data.fetch_api_data",
-        side_effect=[
-            {"query": {"logevents": [oldest_mock_log]}},  # oldest
-            {"query": {"logevents": [newest_mock_log]}},  # newest
-            {
-                "query": {
-                    "logevents": sorted(mock_logs, key=lambda x: x.get("timestamp"))
-                },
-            },  # first month
-            {
-                "query": {
-                    "logevents": sorted(
-                        mock_logs, key=lambda x: x.get("timestamp"), reverse=True
-                    )
-                },
-            },  # last month
-        ],
+        "fetch_data.utils.fetch_data_from_api.requests.get", side_effect=mockery
     )
     success = await create_log_observation(1)
     assert success
