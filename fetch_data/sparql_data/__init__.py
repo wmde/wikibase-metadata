f6357f4b0a6872fc6b62ff2a44e5208bd9b9c055
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-31 11:37:10 +0200

    Rename

--- fetch_data/sparql_data/__init__.py
+++ fetch_data/sparql_data/__init__.py
@@ -3,7 +3,7 @@
 from fetch_data.sparql_data.create_connectivity_data_observation import (
     create_connectivity_observation,
 )
-from fetch_data.sparql_data.create_property_popularity_observation import (
+from fetch_data.sparql_data.create_property_popularity_data_observation import (
     create_property_popularity_observation,
 )
 from fetch_data.sparql_data.create_quantity_data_observation import (
