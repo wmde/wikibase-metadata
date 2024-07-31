4ea364b35b035c62c33040e6f25c6396347ce249
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-31 15:31:55 +0200

    test_create_user_observation_empty

---
+++ tests/test_create_user_observation.py
@@ -0,0 +1,17 @@
+"""Test create_user_observation"""
+
+import pytest
+from fetch_data import create_user_observation
+
+
+@pytest.mark.asyncio
+@pytest.mark.user
+async def test_create_user_observation_empty(mocker):
+    """Test No-Data Scenario"""
+
+    mocker.patch(
+        "fetch_data.user_data.fetch_all_user_data.fetch_api_data",
+        side_effect=[{"query": {"allusers": []}}],
+    )
+    success = await create_user_observation(1)
+    assert success
