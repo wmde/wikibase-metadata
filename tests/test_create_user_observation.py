63e1e87c5aa4ae174898657ae18a163af402cc9b
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-31 19:39:41 +0200

    Tests

--- tests/test_create_user_observation.py
+++ tests/test_create_user_observation.py
@@ -5,6 +5,25 @@ from requests import ReadTimeout
 from fetch_data import create_user_observation
 
 
+TEST_USER_GROUPS = [
+    "private",
+    "bureaucrat",
+    "interface-admin",
+    "sysop",
+    "bot",
+    "data",
+    "push-subscription-manager",
+    "suppress",
+    "editor",
+    "administrator",
+    "translationadmin",
+    "contributor",
+    "checkuser",
+]
+
+TEST_USER_GROUPS_IMPLICIT = {"*", "user", "autoconfirmed"}
+
+
 @pytest.mark.asyncio
 @pytest.mark.user
 async def test_create_user_observation_empty(mocker):
@@ -21,7 +40,7 @@ async def test_create_user_observation_empty(mocker):
 @pytest.mark.asyncio
 @pytest.mark.user
 async def test_create_user_observation_failure(mocker):
-    """Test No-Data Scenario"""
+    """Test Error Scenario"""
 
     mocker.patch(
         "fetch_data.user_data.fetch_all_user_data.fetch_api_data",
@@ -29,3 +48,63 @@ async def test_create_user_observation_failure(mocker):
     )
     success = await create_user_observation(1)
     assert success is False
+
+
+@pytest.mark.asyncio
+@pytest.mark.user
+async def test_create_user_observation_single_pull(mocker):
+    """Test Data, Single Pull Scenario"""
+
+    users = []
+    for i in range(20):
+        users.append(
+            {
+                "groups": sorted(
+                    TEST_USER_GROUPS_IMPLICIT
+                    | {TEST_USER_GROUPS[i % len(TEST_USER_GROUPS)]}
+                    | {TEST_USER_GROUPS[(2 * i) % len(TEST_USER_GROUPS)]}
+                ),
+                "implicitgroups": sorted(TEST_USER_GROUPS_IMPLICIT),
+            }
+        )
+
+    mocker.patch(
+        "fetch_data.user_data.fetch_all_user_data.fetch_api_data",
+        side_effect=[{"query": {"allusers": users}}],
+    )
+    success = await create_user_observation(1)
+    assert success
+
+
+@pytest.mark.asyncio
+@pytest.mark.user
+async def test_create_user_observation_multiple_pull(mocker):
+    """Test Data, Multiple Pull Scenario"""
+
+    users = []
+    for i in range(2000):
+        users.append(
+            {
+                "groups": sorted(
+                    TEST_USER_GROUPS_IMPLICIT
+                    | {TEST_USER_GROUPS[i % len(TEST_USER_GROUPS)]}
+                    | {TEST_USER_GROUPS[(2 * i) % len(TEST_USER_GROUPS)]}
+                ),
+                "implicitgroups": sorted(TEST_USER_GROUPS_IMPLICIT),
+            }
+        )
+
+    limit = 500
+    user_chunks = []
+    for i in range(0, len(users), limit):
+        chunk = {"query": {"allusers": users[i : i + limit]}}
+        if i + limit < len(users):
+            chunk["continue"] = {"aufrom": users[i + limit]}
+        user_chunks.append(chunk)
+
+    mocker.patch(
+        "fetch_data.user_data.fetch_all_user_data.fetch_api_data",
+        side_effect=user_chunks,
+    )
+    success = await create_user_observation(1)
+    assert success
