8e483b87c0ae90a95f2e7cb3dbc1e26908bffb73
Ricki Jay <ricki.jay@wikimedia.de>
2024-07-31 19:23:46 +0200

    Failure

--- tests/test_create_user_observation.py
+++ tests/test_create_user_observation.py
@@ -1,6 +1,7 @@
 """Test create_user_observation"""
 
 import pytest
+from requests import ReadTimeout
 from fetch_data import create_user_observation
 
 
@@ -15,3 +16,16 @@ async def test_create_user_observation_empty(mocker):
     )
     success = await create_user_observation(1)
     assert success
+
+
+@pytest.mark.asyncio
+@pytest.mark.user
+async def test_create_user_observation_failure(mocker):
+    """Test No-Data Scenario"""
+
+    mocker.patch(
+        "fetch_data.user_data.fetch_all_user_data.fetch_api_data",
+        side_effect=[ReadTimeout()],
+    )
+    success = await create_user_observation(1)
+    assert success is False
