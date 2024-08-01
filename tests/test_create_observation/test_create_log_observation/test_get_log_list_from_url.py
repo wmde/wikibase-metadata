b98cd743e17ec8e2d9efd49718e345c3daec0892
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-01 11:40:20 +0200

    Import

--- tests/test_create_observation/test_create_log_observation/test_get_log_list_from_url.py
+++ tests/test_create_observation/test_create_log_observation/test_get_log_list_from_url.py
@@ -6,7 +6,9 @@ from typing import Optional
 from freezegun import freeze_time
 import pytest
 from fetch_data.log_data.fetch_log_data import get_log_list_from_url
-from tests.test_create_log_observation.assert_log_record import assert_log_record
+from tests.test_create_observation.test_create_log_observation.assert_log_record import (
+    assert_log_record,
+)
 
 
 @pytest.mark.log
