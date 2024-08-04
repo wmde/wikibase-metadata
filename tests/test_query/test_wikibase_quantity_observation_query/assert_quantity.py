9d94ae2c8eee8ebc1ced31a1074c48ccc63d3336
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-04 14:52:21 +0200

    Quantity

---
+++ tests/test_query/test_wikibase_quantity_observation_query/assert_quantity.py
@@ -0,0 +1,23 @@
+"""Assert Quantity Observation"""
+
+from tests.utils import assert_property_value
+
+
+def assert_quantity(
+    returned_quantity: dict,
+    expected_id: str,
+    expected_returned_data: bool,
+    expected_items: int,
+    expected_lexemes: int,
+    expected_properties: int,
+    expected_triples: int,
+):
+    """Assert Quantity Observation"""
+
+    assert_property_value(returned_quantity, "id", expected_id)
+    assert "observationDate" in returned_quantity
+    assert_property_value(returned_quantity, "returnedData", expected_returned_data)
+    assert_property_value(returned_quantity, "totalItems", expected_items)
+    assert_property_value(returned_quantity, "totalLexemes", expected_lexemes)
+    assert_property_value(returned_quantity, "totalProperties", expected_properties)
+    assert_property_value(returned_quantity, "totalTriples", expected_triples)
---
+++ tests/test_query/test_wikibase_quantity_observation_query/test_wikibase_quantity_most_recent_observation_query.py
@@ -0,0 +1,53 @@
+"""Test Wikibase Most Recent Quantity Observation Query"""
+
+import pytest
+from tests.test_query.test_wikibase_quantity_observation_query.assert_quantity import (
+    assert_quantity,
+)
+from tests.test_query.test_wikibase_quantity_observation_query.wikibase_quantity_observation_fragment import (
+    WIKIBASE_QUANTITY_OBSERVATION_FRAGMENT,
+)
+from tests.test_schema import test_schema
+from tests.utils import assert_property_value
+
+
+WIKIBASE_QUANTITY_MOST_RECENT_OBSERVATION_QUERY = (
+    """
+query MyQuery($wikibaseId: Int!) {
+  wikibase(wikibaseId: $wikibaseId) {
+    id
+    quantityObservations {
+      mostRecent {
+        ...WikibaseQuantityObservationStrawberryModelFragment
+      }
+    }
+  }
+}
+
+"""
+    + WIKIBASE_QUANTITY_OBSERVATION_FRAGMENT
+)
+
+
+@pytest.mark.asyncio
+@pytest.mark.dependency(depends_on=["quantity-success"])
+@pytest.mark.query
+@pytest.mark.quantity
+async def test_wikibase_quantity_most_recent_observation_query():
+    """Test Wikibase Quantity Most Recent Observation"""
+
+    result = await test_schema.execute(
+        WIKIBASE_QUANTITY_MOST_RECENT_OBSERVATION_QUERY,
+        variable_values={"wikibaseId": 1},
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+    assert "wikibase" in result.data
+    result_wikibase = result.data["wikibase"]
+    assert_property_value(result_wikibase, "id", "1")
+    assert "quantityObservations" in result_wikibase
+    assert "mostRecent" in result_wikibase["quantityObservations"]
+    most_recent = result_wikibase["quantityObservations"]["mostRecent"]
+
+    assert_quantity(most_recent, "1", True, 2, 4, 1, 8)
