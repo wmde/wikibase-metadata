03068a362e39c996e0e37be879867f9b21f2cb23
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-06 19:15:22 +0200

    Agg Quantity

---
+++ tests/test_query/aggregation/test_aggregate_quantity_query.py
@@ -0,0 +1,45 @@
+"""Test Aggregate Quantity Query"""
+
+import pytest
+from tests.test_schema import test_schema
+from tests.utils import assert_layered_property_value
+
+
+AGGREGATED_QUANTITY_QUERY = """
+query MyQuery {
+  aggregateQuantity {
+    totalItems
+    totalLexemes
+    totalProperties
+    totalTriples
+    wikibaseCount
+  }
+}
+"""
+
+
+@pytest.mark.asyncio
+@pytest.mark.agg
+@pytest.mark.dependency(depends=["quantity-success"], scope="session")
+@pytest.mark.quantity
+@pytest.mark.query
+async def test_aggregate_quantity_query():
+    """Test Aggregate Quantity Query"""
+
+    result = await test_schema.execute(
+        AGGREGATED_QUANTITY_QUERY,
+        variable_values={"pageNumber": 1, "pageSize": 30},
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+
+    assert_layered_property_value(result.data, ["aggregateQuantity", "totalItems"], 2)
+    assert_layered_property_value(result.data, ["aggregateQuantity", "totalLexemes"], 4)
+    assert_layered_property_value(
+        result.data, ["aggregateQuantity", "totalProperties"], 1
+    )
+    assert_layered_property_value(result.data, ["aggregateQuantity", "totalTriples"], 8)
+    assert_layered_property_value(
+        result.data, ["aggregateQuantity", "wikibaseCount"], 1
+    )
