1ff35a70f34548e1af23b3462bd3c7ce51d34710
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-06 15:38:59 +0200

    Extensions Page II

--- tests/test_query/aggregation/software_version_obs/test_aggregate_extensions_query.py
+++ tests/test_query/aggregation/software_version_obs/test_aggregate_extensions_query.py
@@ -29,7 +29,7 @@ query MyQuery($pageNumber: Int!, $pageSize: Int!) {
 @pytest.mark.dependency(depends=["software-version-success"], scope="session")
 @pytest.mark.query
 @pytest.mark.version
-async def test_aggregate_extensions_query():
+async def test_aggregate_extensions_query_page_one():
     """Test Aggregated Extensions Query"""
 
     result = await test_schema.execute(
@@ -86,6 +86,91 @@ async def test_aggregate_extensions_query():
                 "cb0a218",
             ),
             ("Scribunto", None, (None, None, None), None, None),
+        ]
+    ):
+
+        assert_software_version_aggregate(
+            result.data["aggregateExtensionPopularity"]["data"][index],
+            expected_software_name,
+            expected_version_string,
+            expected_version_semver,
+            expected_version_date,
+            expected_version_hash,
+        )
+
+
+@pytest.mark.asyncio
+@pytest.mark.dependency(depends=["software-version-success"], scope="session")
+@pytest.mark.query
+@pytest.mark.version
+async def test_aggregate_extensions_query_page_two():
+    """Test Aggregated Extensions Query"""
+
+    result = await test_schema.execute(
+        AGGREGATE_EXTENSIONS_QUERY, variable_values={"pageNumber": 2, "pageSize": 5}
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+    assert_layered_property_value(
+        result.data, ["aggregateExtensionPopularity", "meta", "pageNumber"], 2
+    )
+    assert_layered_property_value(
+        result.data, ["aggregateExtensionPopularity", "meta", "pageSize"], 5
+    )
+    assert_layered_property_value(
+        result.data, ["aggregateExtensionPopularity", "meta", "totalCount"], 10
+    )
+    assert_layered_property_value(
+        result.data, ["aggregateExtensionPopularity", "meta", "totalPages"], 2
+    )
+    assert_layered_property_count(
+        result.data, ["aggregateExtensionPopularity", "data"], 5
+    )
+
+    for index, (
+        expected_software_name,
+        expected_version_string,
+        expected_version_semver,
+        expected_version_date,
+        expected_version_hash,
+    ) in enumerate(
+        [
+            (
+                "UniversalLanguageSelector",
+                "2020-01-23",
+                (None, None, None),
+                datetime(2020, 3, 3, 13, 38),
+                "61f1a98",
+            ),
+            (
+                "WikibaseClient",
+                "dbbcdd8",
+                (None, None, None),
+                datetime(2019, 12, 10, 12, 52),
+                "dbbcdd8",
+            ),
+            (
+                "WikibaseLib",
+                "dbbcdd8",
+                (None, None, None),
+                datetime(2019, 12, 10, 12, 52),
+                "dbbcdd8",
+            ),
+            (
+                "WikibaseRepository",
+                "dbbcdd8",
+                (None, None, None),
+                datetime(2019, 12, 10, 12, 52),
+                "dbbcdd8",
+            ),
+            (
+                "WikibaseView",
+                "dbbcdd8",
+                (None, None, None),
+                datetime(2019, 12, 10, 12, 52),
+                "dbbcdd8",
+            ),
         ]
     ):
 
