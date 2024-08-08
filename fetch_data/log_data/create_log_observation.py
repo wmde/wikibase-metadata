7a88efb8c61b82696bd51cbc6276d4fe4b0c2b2e
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-08 13:26:19 +0200

    Error

--- fetch_data/log_data/create_log_observation.py
+++ fetch_data/log_data/create_log_observation.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from json.decoder import JSONDecodeError
 from typing import List
-from requests.exceptions import ReadTimeout, SSLError
+from requests.exceptions import ConnectionError, ReadTimeout, SSLError
 from data import get_async_session
 from fetch_data.log_data.fetch_log_data import (
     get_log_list_from_url,
@@ -74,7 +74,7 @@ async def create_log_observation(wikibase_id: int) -> bool:
             )
 
             observation.returned_data = True
-        except (JSONDecodeError, ReadTimeout, SSLError):
+        except (ConnectionError, JSONDecodeError, ReadTimeout, SSLError):
             observation.returned_data = False
 
         wikibase.log_observations.append(observation)
