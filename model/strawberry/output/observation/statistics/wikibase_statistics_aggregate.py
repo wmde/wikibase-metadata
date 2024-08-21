8b2a2bd5e9cc51103aa1f5cc5b72bb3c11b2ccd3
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-21 21:14:11 +0200

    Tree

--- model/strawberry/output/observation/statistics/wikibase_statistics_aggregate.py
+++ model/strawberry/output/observation/statistics/wikibase_statistics_aggregate.py
@@ -1,46 +1,25 @@
 """Aggregate Statistics Strawberry Model"""
 
-from typing import Optional
 import strawberry
 
-from model.strawberry.scalars import BigInt
+from model.strawberry.output.observation.statistics.component import (
+    WikibaseStatisticsEditsObservationStrawberryModel,
+    WikibaseStatisticsFilesObservationStrawberryModel,
+    WikibaseStatisticsPagesObservationStrawberryModel,
+    WikibaseStatisticsUsersObservationStrawberryModel,
+)
 
 
 @strawberry.type
 class WikibaseStatisticsAggregateStrawberryModel:  # pylint: disable=too-many-instance-attributes
     """Aggregate Statistics"""
 
-    total_pages: int = strawberry.field(description="Total Pages", graphql_type=BigInt)
-    content_pages: int = strawberry.field(
-        description="Content Pages", graphql_type=BigInt
-    )
-    total_files: int = strawberry.field(description="Total Files", graphql_type=BigInt)
-    total_edits: int = strawberry.field(description="Total Edits", graphql_type=BigInt)
-    content_page_word_count_total: int = strawberry.field(
-        description="Total Content Word Count", graphql_type=BigInt
-    )
-    total_users: int = strawberry.field(description="Total Users", graphql_type=BigInt)
-    active_users: int = strawberry.field(
-        description="Active Users", graphql_type=BigInt
-    )
-    total_admin: int = strawberry.field(description="Total Admin", graphql_type=BigInt)
+    edits: WikibaseStatisticsEditsObservationStrawberryModel
+    files: WikibaseStatisticsFilesObservationStrawberryModel
+    pages: WikibaseStatisticsPagesObservationStrawberryModel
+    users: WikibaseStatisticsUsersObservationStrawberryModel
+
     wikibase_count: int = strawberry.field(description="Wikibases with Statistics Data")
-
-    @strawberry.field(description="Content Page Word Count - Average")
-    def content_page_word_count_avg(self) -> Optional[float]:
-        """Average Word Count per Content Page"""
-
-        if self.content_pages == 0:
-            return None
-        return self.content_page_word_count_total / self.content_pages
-
-    @strawberry.field(description="Average Edits per Page")
-    def edits_per_page_avg(self) -> Optional[float]:
-        """Average Edits per Page"""
-
-        if self.total_pages == 0:
-            return None
-        return self.total_edits / self.total_pages
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
@@ -54,12 +33,14 @@ class WikibaseStatisticsAggregateStrawberryModel:  # pylint: disable=too-many-in
         total_admin: int,
         wikibase_count: int,
     ) -> "WikibaseStatisticsAggregateStrawberryModel":
+        self.edits = WikibaseStatisticsEditsObservationStrawberryModel(
+            total_edits, total_pages
+        )
+        self.files = WikibaseStatisticsFilesObservationStrawberryModel(total_files)
+        self.pages = WikibaseStatisticsPagesObservationStrawberryModel(
+            total_pages, content_pages, content_page_word_count_total
+        )
+        self.users = WikibaseStatisticsUsersObservationStrawberryModel(
+            total_users, active_users, total_admin
+        )
         self.wikibase_count = wikibase_count
-        self.total_pages = total_pages
-        self.content_pages = content_pages
-        self.total_files = total_files
-        self.total_edits = total_edits
-        self.content_page_word_count_total = content_page_word_count_total
-        self.total_users = total_users
-        self.active_users = active_users
-        self.total_admin = total_admin
