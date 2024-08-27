0cc27cbf6b7989fa7aad82a5053ad99a6db85e20
Ricki Jay <ricki.jay@wikimedia.de>
2024-08-27 17:57:06 +0200

    test

--- tests/test_data_expectations.py
+++ tests/test_data_expectations.py
@@ -12,11 +12,10 @@ CHECKPOINT_DIRECTORY = "./data/gx/checkpoints"
 # Define the pytest_generate_tests hook to generate test cases
 def pytest_generate_tests(metafunc):
     """Add Parameters"""
-    checkpoints = [Path(file).stem for file in os.listdir(CHECKPOINT_DIRECTORY)]
 
     if "checkpoint_name" in metafunc.fixturenames:
         # Generate test cases based on the test_data list
-        metafunc.parametrize("checkpoint_name", sorted(checkpoints))
+        metafunc.parametrize("checkpoint_name", sorted(os.listdir(CHECKPOINT_DIRECTORY)))
 
 
 @pytest.mark.data
@@ -25,7 +24,7 @@ def test_data_expectations(checkpoint_name: str):
 
     print(f"\n{checkpoint_name}")
     context = gx.get_context(project_root_dir=GREAT_EXPECTATIONS_PROJECT_ROOT)
-    retrieved_checkpoint = context.get_checkpoint(name=checkpoint_name)
+    retrieved_checkpoint = context.checkpoints.get(checkpoint_name)
     result = retrieved_checkpoint.run()
     assert result.success, result.run_results
 
