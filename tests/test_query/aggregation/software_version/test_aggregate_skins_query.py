2b5fe2d330aed9067181f46fda6873f2419e335b
Ricki Jay <ricki.jay@wikimedia.de>
2024-09-23 13:45:05 +0200

    Use

--- tests/test_query/aggregation/software_version/test_aggregate_skins_query.py
+++ tests/test_query/aggregation/software_version/test_aggregate_skins_query.py
@@ -8,7 +8,7 @@ from tests.test_query.aggregation.software_version.software_version_aggregate_fr
     SOFTWARE_VERSION_DOUBLE_AGGREGATE_FRAGMENT,
 )
 from tests.test_schema import test_schema
-from tests.utils import assert_layered_property_count, assert_layered_property_value
+from tests.utils import assert_layered_property_count, assert_page_meta
 
 
 AGGREGATE_SKINS_QUERY = (
@@ -38,17 +38,12 @@ async def test_aggregate_skins_query_page_one():
 
     assert result.errors is None
     assert result.data is not None
-    assert_layered_property_value(
-        result.data, ["aggregateSkinPopularity", "meta", "pageNumber"], 1
-    )
-    assert_layered_property_value(
-        result.data, ["aggregateSkinPopularity", "meta", "pageSize"], 5
-    )
-    assert_layered_property_value(
-        result.data, ["aggregateSkinPopularity", "meta", "totalCount"], 3
-    )
-    assert_layered_property_value(
-        result.data, ["aggregateSkinPopularity", "meta", "totalPages"], 1
+    assert_page_meta(
+        result.data["aggregateSkinPopularity"],
+        expected_page_number=1,
+        expected_page_size=5,
+        expected_total_count=3,
+        expected_total_pages=1,
     )
     assert_layered_property_count(result.data, ["aggregateSkinPopularity", "data"], 3)
 
--- tests/test_query/aggregation/test_aggregate_property_popularity_query.py
+++ tests/test_query/aggregation/test_aggregate_property_popularity_query.py
@@ -2,7 +2,11 @@
 
 import pytest
 from tests.test_schema import test_schema
-from tests.utils import assert_layered_property_count, assert_layered_property_value
+from tests.utils import (
+    assert_layered_property_count,
+    assert_layered_property_value,
+    assert_page_meta,
+)
 
 
 AGGREGATED_PROPERTY_POPULARITY_QUERY = """
@@ -41,19 +45,13 @@ async def test_aggregate_property_popularity_query():
     assert result.errors is None
     assert result.data is not None
 
-    assert_layered_property_value(
-        result.data, ["aggregatePropertyPopularity", "meta", "pageNumber"], 1
+    assert_page_meta(
+        result.data["aggregatePropertyPopularity"],
+        expected_page_number=1,
+        expected_page_size=30,
+        expected_total_count=2,
+        expected_total_pages=1,
     )
-    assert_layered_property_value(
-        result.data, ["aggregatePropertyPopularity", "meta", "pageSize"], 30
-    )
-    assert_layered_property_value(
-        result.data, ["aggregatePropertyPopularity", "meta", "totalCount"], 2
-    )
-    assert_layered_property_value(
-        result.data, ["aggregatePropertyPopularity", "meta", "totalPages"], 1
-    )
-
     assert_layered_property_count(
         result.data, ["aggregatePropertyPopularity", "data"], 2
     )
