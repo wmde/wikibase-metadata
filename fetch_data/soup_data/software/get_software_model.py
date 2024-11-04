6b826790b5908aba3c3e30f7d1b589a39a0e2511
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-04 17:18:26 +0100

    Legibility

--- fetch_data/soup_data/software/get_software_model.py
+++ fetch_data/soup_data/software/get_software_model.py
@@ -18,7 +18,33 @@ async def get_or_create_software_model(
     if re.match(EXTENSIONNAME_PATTERN, software_name):
         software_name = re.sub(EXTENSIONNAME_PATTERN, r"\1", software_name)
 
-    existing = (
+    existing = await get_existing_software_model(
+        async_session, software_type, software_name
+    )
+    if existing is not None:
+        return existing
+
+    nearby = await get_nearby_software_model(
+        async_session, software_type, software_name
+    )
+    if nearby is not None:
+        return nearby
+
+    creating = WikibaseSoftwareModel(
+        software_type=software_type, software_name=software_name
+    )
+    async_session.add(creating)
+    await async_session.flush()
+    await async_session.refresh(creating)
+    return creating
+
+
+async def get_existing_software_model(
+    async_session: AsyncSession, software_type: WikibaseSoftwareType, software_name: str
+) -> Optional[WikibaseSoftwareModel]:
+    """Return Existing Software, If Existent"""
+
+    return (
         await async_session.scalars(
             select(WikibaseSoftwareModel).where(
                 and_(
@@ -28,8 +54,18 @@ async def get_or_create_software_model(
             )
         )
     ).one_or_none()
-    if existing is not None:
-        return existing
+
+
+async def get_nearby_software_model(
+    async_session: AsyncSession, software_type: WikibaseSoftwareType, software_name: str
+) -> Optional[WikibaseSoftwareModel]:
+    """
+    Return Matching Software, if found
+
+    Only allowable differences: whitespace and capitalization
+
+    Assert only one or zero matches
+    """
 
     all_software_of_type = (
         await async_session.scalars(
@@ -48,13 +84,4 @@ async def get_or_create_software_model(
             nearby = s
             nearby_count += 1
     assert nearby_count <= 1
-    if nearby is not None:
-        return nearby
-
-    creating = WikibaseSoftwareModel(
-        software_type=software_type, software_name=software_name
-    )
-    async_session.add(creating)
-    await async_session.flush()
-    await async_session.refresh(creating)
-    return creating
+    return nearby
