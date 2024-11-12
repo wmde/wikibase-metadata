0a5b8d344fdf53dd8a3156d44db405859436ccfe
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-12 14:24:35 +0100

    Lint

--- tests/test_create_observation/test_create_software_version_observation/test_update_software_data.py
+++ tests/test_create_observation/test_create_software_version_observation/test_update_software_data.py
@@ -21,7 +21,7 @@ from tests.utils import MockResponse
 async def test_update_software_data(mocker):
     """Test Update Software Data"""
 
-    # pylint: disable=unused-argument
+    # pylint: disable=unused-argument,too-many-return-statements
     def mockery(*args, **kwargs):
         print(args)
         query = args[0]
@@ -128,6 +128,7 @@ def test_get_update_extension_query():
 
     query = get_update_extension_query()
     assert str(query) == (
+        # pylint: disable=line-too-long
         """SELECT wikibase_software.id, wikibase_software.software_type, wikibase_software.software_name, wikibase_software.url, wikibase_software.fetched, wikibase_software.description, wikibase_software.latest_version, wikibase_software.quarterly_download_count, wikibase_software.public_wiki_count, wikibase_software.mw_bundled, wikibase_software.archived 
 FROM wikibase_software 
 WHERE (wikibase_software.fetched IS NULL OR wikibase_software.fetched < :fetched_1) AND wikibase_software.software_type = :software_type_1 AND (wikibase_software.archived = false OR wikibase_software.archived IS NULL)
