3533ea1b520e57cc56ce7ac7f53dcc1429fc80e7
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-15 12:25:21 +0100

    Tests

--- fetch_data/utils/clean_string.py
+++ fetch_data/utils/clean_string.py
@@ -7,5 +7,8 @@ def clean_string(input_str: str) -> str:
     """Clean String"""
 
     stripped = re.sub(r"[ ]{2,}|[ ]", r" ", input_str).strip()
+    if len(stripped) < 2:
+        return stripped.upper()
+    
     first_cap = stripped[0].upper() + stripped[1:]
     return first_cap
