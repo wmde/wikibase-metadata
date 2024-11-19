8b3642c63795bcd2c190e20cffcff22201b58f13
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-19 12:10:05 +0100

    Initial Tests

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 """Main Application"""
 
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
+from fetch_data.out_of_date.update_out_of_date import (
+    update_out_of_date_stats_observations,
+)
 from model.strawberry import schema
 
-app = FastAPI()
+
+# Set up the scheduler
+scheduler = AsyncIOScheduler()
+# four_minute_trigger = IntervalTrigger(minutes=4)
+hourly_trigger = CronTrigger(minute=6)  # every hour
+scheduler.add_job(update_out_of_date_stats_observations, hourly_trigger)
+
+
+# Ensure the scheduler shuts down properly on application exit.
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 origins = ["http://localhost", "http://0.0.0.0", "http://127.0.0.1"]
 
