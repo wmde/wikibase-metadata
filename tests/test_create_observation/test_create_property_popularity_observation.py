08a23b98cfd43e92f0fc891dbc931c4f43795f80
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-19 21:27:02 +0100

    Enforce Order

--- tests/test_create_observation/test_create_property_popularity_observation.py
+++ tests/test_create_observation/test_create_property_popularity_observation.py
@@ -1,5 +1,7 @@
 """Test create_property_popularity_observation"""
 
+import asyncio
+import time
 from urllib.error import HTTPError
 import pytest
 from fetch_data import create_property_popularity_observation
@@ -15,6 +17,8 @@ from fetch_data import create_property_popularity_observation
 @pytest.mark.sparql
 async def test_create_property_popularity_observation_success(mocker):
     """Test One-Pull Per Month, Data Returned Scenario"""
+
+    await asyncio.to_thread(time.sleep, 1)
 
     mocker.patch(
         "fetch_data.sparql_data.create_property_popularity_data_observation.get_sparql_results",
@@ -35,7 +39,9 @@ async def test_create_property_popularity_observation_success(mocker):
 
 @pytest.mark.asyncio
 @pytest.mark.dependency(
-    name="property-popularity-failure", depends=["add-wikibase"], scope="session"
+    name="property-popularity-failure",
+    depends=["property-popularity-success"],
+    scope="session",
 )
 @pytest.mark.property
 @pytest.mark.sparql
