7d95ab240985413695130578266b39b4c09188f9
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-19 12:09:10 +0100

    Update

--- fetch_data/out_of_date/__init__.py
+++ fetch_data/out_of_date/__init__.py
@@ -10,3 +10,13 @@ from fetch_data.out_of_date.get_out_of_date_wikibases import (
     get_wikibase_list_with_out_of_date_stats_observations,
     get_wikibase_list_with_out_of_date_user_observations,
 )
+from fetch_data.out_of_date.update_out_of_date import (
+    update_out_of_date_connectivity_observations,
+    update_out_of_date_log_first_observations,
+    update_out_of_date_log_last_observations,
+    update_out_of_date_property_observations,
+    update_out_of_date_quantity_observations,
+    update_out_of_date_software_observations,
+    update_out_of_date_stats_observations,
+    update_out_of_date_user_observations,
+)
