06cfac83703538a599e2e092873414e6697b793f
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-19 12:37:44 +0100

    Try Everything

--- app.py
+++ app.py
@@ -3,22 +3,52 @@
 from contextlib import asynccontextmanager
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from fetch_data.out_of_date.update_out_of_date import (
+    update_out_of_date_connectivity_observations,
+    update_out_of_date_log_first_observations,
+    update_out_of_date_log_last_observations,
+    update_out_of_date_property_observations,
+    update_out_of_date_quantity_observations,
+    update_out_of_date_software_observations,
     update_out_of_date_stats_observations,
+    update_out_of_date_user_observations,
 )
 from model.strawberry import schema
 
 
 # Set up the scheduler
 scheduler = AsyncIOScheduler()
-# four_minute_trigger = IntervalTrigger(minutes=4)
-hourly_trigger = CronTrigger(minute=6)  # every hour
-scheduler.add_job(update_out_of_date_stats_observations, hourly_trigger)
+
+start_min = 14
+
+scheduler.add_job(
+    update_out_of_date_connectivity_observations, CronTrigger(minute=0 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_log_first_observations, CronTrigger(minute=5 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_log_last_observations, CronTrigger(minute=10 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_property_observations, CronTrigger(minute=15 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_quantity_observations, CronTrigger(minute=20 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_software_observations, CronTrigger(minute=25 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_stats_observations, CronTrigger(minute=30 + start_min)
+)
+scheduler.add_job(
+    update_out_of_date_user_observations, CronTrigger(minute=35 + start_min)
+)
 
 
 # Ensure the scheduler shuts down properly on application exit.
