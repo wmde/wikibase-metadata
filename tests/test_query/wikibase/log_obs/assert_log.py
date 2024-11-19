8a9275cb3e0aa7ad3f6fe48e4ec6fa68bdfc6a4f
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-19 01:03:16 +0100

    Unused

--- tests/test_query/wikibase/log_obs/assert_log.py
+++ tests/test_query/wikibase/log_obs/assert_log.py
@@ -2,48 +2,7 @@
 
 from datetime import datetime
 from typing import Optional
-from tests.utils import (
-    assert_layered_property_count,
-    assert_layered_property_value,
-    assert_property_value,
-    DATETIME_FORMAT,
-)
-
-
-# pylint: disable=too-many-arguments,too-many-positional-arguments
-def assert_month_record(
-    month_record: dict,
-    expected_id: str,
-    expected_first_log_date: datetime,
-    expected_last_log_date: datetime,
-    expected_log_count: int,
-    expected_user_count: int,
-    expected_human_count: int,
-    expected_log_type_record_count: int,
-    expected_user_type_record_count: int,
-):
-    """Assert Month Record"""
-
-    assert_property_value(month_record, "id", expected_id)
-    assert_layered_property_value(
-        month_record,
-        ["firstLog", "date"],
-        expected_first_log_date.strftime(DATETIME_FORMAT),
-    )
-    assert_layered_property_value(
-        month_record,
-        ["lastLog", "date"],
-        expected_last_log_date.strftime(DATETIME_FORMAT),
-    )
-    assert_property_value(month_record, "logCount", expected_log_count)
-    assert_property_value(month_record, "allUsers", expected_user_count)
-    assert_property_value(month_record, "humanUsers", expected_human_count)
-    assert_layered_property_count(
-        month_record, ["logTypeRecords"], expected_log_type_record_count
-    )
-    assert_layered_property_count(
-        month_record, ["userTypeRecords"], expected_user_type_record_count
-    )
+from tests.utils import assert_property_value, DATETIME_FORMAT
 
 
 # pylint: disable=too-many-arguments,too-many-positional-arguments
