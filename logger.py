662725ce48e9a4afd749036e4d018cdc4c424cdb
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-21 12:23:16 +0100

    Basic Formatting

--- logger.py
+++ logger.py
@@ -12,6 +12,8 @@ os.makedirs("logs/error", exist_ok=True)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+format = logging.Formatter(fmt='%(asctime)s %(message)s')
+
 file_handler = TimedRotatingFileHandler(
     f"logs/{log_level.lower()}/suite-scraper.log",
     encoding="utf-8",
@@ -19,10 +21,12 @@ file_handler = TimedRotatingFileHandler(
     utc=True,
 )
 file_handler.setLevel(log_level.upper())
+file_handler.setFormatter(format)
 logger.addHandler(file_handler)
 
 file_error_handler = TimedRotatingFileHandler(
     "logs/error/suite-scraper-error.log", encoding="utf-8", when="w0", utc=True
 )
 file_error_handler.setLevel(logging.WARNING)
+file_error_handler.setFormatter(format)
 logger.addHandler(file_error_handler)
