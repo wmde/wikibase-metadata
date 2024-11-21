1473df5f71c8eae07faf45292c20183b929bba65
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-21 11:18:51 +0100

    Logger & Config

---
+++ logger.py
@@ -0,0 +1,18 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+
+from config import log_level
+
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = TimedRotatingFileHandler("logs/suite-scraper.log", encoding='utf-8', when='midnight', utc=True)
+file_handler.setLevel(log_level.upper())
+logger.addHandler(file_handler)
+
+file_error_handler = TimedRotatingFileHandler("logs/suite-scraper-error.log", encoding='utf-8', when='w0', utc=True)
+file_error_handler.setLevel(logging.WARNING)
+logger.addHandler(file_error_handler)
