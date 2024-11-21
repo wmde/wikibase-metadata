55bc5b76a506d9ca781073d9dd9672e1a465e25f
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-21 11:51:43 +0100

    Organize

--- logger.py
+++ logger.py
@@ -6,19 +6,23 @@ import os
 
 from config import log_level
 
-os.makedirs("logs", exist_ok=True)
+os.makedirs(f"logs/{log_level.lower()}", exist_ok=True)
+os.makedirs("logs/error", exist_ok=True)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 file_handler = TimedRotatingFileHandler(
-    "logs/suite-scraper.log", encoding="utf-8", when="midnight", utc=True
+    f"logs/{log_level.lower()}/suite-scraper.log",
+    encoding="utf-8",
+    when="midnight",
+    utc=True,
 )
 file_handler.setLevel(log_level.upper())
 logger.addHandler(file_handler)
 
 file_error_handler = TimedRotatingFileHandler(
-    "logs/suite-scraper-error.log", encoding="utf-8", when="w0", utc=True
+    "logs/error/suite-scraper-error.log", encoding="utf-8", when="w0", utc=True
 )
 file_error_handler.setLevel(logging.WARNING)
 logger.addHandler(file_error_handler)
