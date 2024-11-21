80ab5e4923f68adffcb1873f526944cedbd13890
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-21 12:31:11 +0100

    Be a Clown

--- logger.py
+++ logger.py
@@ -6,13 +6,30 @@ import os
 
 from config import log_level
 
+
+class OptionalExtraFormatter(logging.Formatter):
+    """Supports an optional `wikibase` extra argument"""
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format Record"""
+
+        if "wikibase" in record.__dict__.keys():
+            self._style._fmt = "%(asctime)s | wikibase %(wikibase)d | %(message)s"  # pylint: disable=protected-access
+        else:
+            self._style._fmt = (  # pylint: disable=protected-access
+                "%(asctime)s | %(message)s"
+            )
+
+        return super().format(record)
+
+
 os.makedirs(f"logs/{log_level.lower()}", exist_ok=True)
 os.makedirs("logs/error", exist_ok=True)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-format = logging.Formatter(fmt='%(asctime)s %(message)s')
+wikibase_formatter = OptionalExtraFormatter()
 
 file_handler = TimedRotatingFileHandler(
     f"logs/{log_level.lower()}/suite-scraper.log",
@@ -21,12 +38,12 @@ file_handler = TimedRotatingFileHandler(
     utc=True,
 )
 file_handler.setLevel(log_level.upper())
-file_handler.setFormatter(format)
+file_handler.setFormatter(wikibase_formatter)
 logger.addHandler(file_handler)
 
 file_error_handler = TimedRotatingFileHandler(
     "logs/error/suite-scraper-error.log", encoding="utf-8", when="w0", utc=True
 )
 file_error_handler.setLevel(logging.WARNING)
-file_error_handler.setFormatter(format)
+file_error_handler.setFormatter(wikibase_formatter)
 logger.addHandler(file_error_handler)
