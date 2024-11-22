97734802136d3b98cdbcb874539de8fc2c984e95
Ricki Jay <ricki.jay@wikimedia.de>
2024-11-22 08:10:05 +0100

    disable-next

--- resolvers/get_aggregate_created.py
+++ resolvers/get_aggregate_created.py
@@ -40,7 +40,7 @@ def get_created_query() -> Select[tuple[int, int]]:
             and_(
                 WikibaseLogMonthObservationModel.returned_data,
                 WikibaseLogMonthObservationModel.first_month,
-                # pylint: disable=singleton-comparison
+                # pylint: disable-next=singleton-comparison
                 WikibaseLogMonthObservationModel.first_log_date != None,
                 WikibaseLogMonthObservationModel.wikibase.has(WikibaseModel.checked),
             )
