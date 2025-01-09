dc6ddf9ca01f1eea2b3a4da09e977862ca07ef4a
Ricki <141253288+RickiJay-WMDE@users.noreply.github.com>
2025-01-09 14:27:04 +0100

    Logging Settings (#51)
    * Logging Settings
    
    * Default Value
    
    * Unneeded
    
    * Easier

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ config.read(os.environ.get("SETTINGS_FILE") or "settings.ini")
 database_connection_string = os.path.expandvars(
     config.get("database", "database_connection_string")
 )
-log_level = os.path.expandvars(config.get("logging", "log_level"))
+
+log_level = config.get("logging", "log_level", fallback="INFO")
