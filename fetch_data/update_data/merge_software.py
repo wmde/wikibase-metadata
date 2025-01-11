0a15fa99dcf5ac0615ac4e30c2a79909c4bb2cf9
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-11 01:15:52 +0100

    Light Refactor

--- fetch_data/update_data/merge_software.py
+++ fetch_data/update_data/merge_software.py
@@ -1,6 +1,6 @@
 """Merge Software"""
 
-from sqlalchemy import and_, delete, select, update
+from sqlalchemy import Select, Update, and_, delete, select, update
 from data.database_connection import get_async_session
 from model.database import (
     WikibaseSoftwareVersionModel,
@@ -11,58 +11,81 @@ from model.database.wikibase_software.software_tag_xref_model import (
 )
 
 
-async def merge_software_by_id(base_id: int, additional_id: int) -> int:
+async def merge_software_by_id(base_id: int, additional_id: int) -> bool:
     """Merge Software by ID"""
 
+    software_query = get_select_software_query([base_id, additional_id])
+    update_software_version_query = get_update_software_version_query(
+        base_id, additional_id
+    )
+    update_software_tags_query = get_update_software_tags_query(base_id, additional_id)
+    delete_additional_tags_query = software_tag_xref_table.delete().where(
+        software_tag_xref_table.c.wikibase_software_id == additional_id
+    )
+    delete_software_query = delete(WikibaseSoftwareModel).where(
+        WikibaseSoftwareModel.id == additional_id
+    )
+
     async with get_async_session() as async_session:
-        software_query = select(WikibaseSoftwareModel.software_type).where(
-            WikibaseSoftwareModel.id.in_([base_id, additional_id])
-        )
-        software_type_list = (await async_session.scalars(software_query)).all()
-        assert len({software_type_list}) == 1
+        software_list = (await async_session.scalars(software_query)).all()
+        assert len({s.software_type for s in software_list}) == 1
 
-        update_software_version_query = (
-            update(WikibaseSoftwareVersionModel)
-            .where(WikibaseSoftwareVersionModel.software_id == additional_id)
-            .values(software_id=base_id)
-        )
         await async_session.execute(update_software_version_query)
-
-        update_software_tags_query = software_tag_xref_table.insert().from_select(
-            [
-                software_tag_xref_table.c.wikibase_software_id,
-                software_tag_xref_table.c.wikibase_software_tag_id,
-            ],
-            select(
-                software_tag_xref_table.c.wikibase_software_id,
-                software_tag_xref_table.c.wikibase_software_tag_id,
-            ).where(
-                and_(
-                    software_tag_xref_table.c.wikibase_software_id == additional_id,
-                    software_tag_xref_table.c.wikibase_software_tag_id.not_in(
-                        select(
-                            software_tag_xref_table.c.wikibase_software_tag_id
-                        ).where(
-                            software_tag_xref_table.c.wikibase_software_id == base_id
-                        )
-                    ),
-                )
-            ),
-        )
         await async_session.execute(update_software_tags_query)
-
-        delete_additional_tags_query = software_tag_xref_table.delete().where(
-            software_tag_xref_table.c.wikibase_software_id == additional_id
-        )
         await async_session.execute(delete_additional_tags_query)
-
         await async_session.flush()
 
-        delete_software_query = delete(WikibaseSoftwareModel).where(
-            WikibaseSoftwareModel.id == additional_id
-        )
         await async_session.execute(delete_software_query)
-
         await async_session.commit()
 
-    return 1
+    async with get_async_session() as async_session:
+        remaining = (await async_session.scalars(software_query)).all()
+        return len(remaining) == 1
+
+
+def get_update_software_tags_query(base_id: int, additional_id: int) -> Update:
+    """Add Additional Software Tags to Base"""
+
+    update_software_tags_query = software_tag_xref_table.insert().from_select(
+        [
+            software_tag_xref_table.c.wikibase_software_id,
+            software_tag_xref_table.c.wikibase_software_tag_id,
+        ],
+        select(
+            software_tag_xref_table.c.wikibase_software_id,
+            software_tag_xref_table.c.wikibase_software_tag_id,
+        ).where(
+            and_(
+                software_tag_xref_table.c.wikibase_software_id == additional_id,
+                software_tag_xref_table.c.wikibase_software_tag_id.not_in(
+                    select(software_tag_xref_table.c.wikibase_software_tag_id).where(
+                        software_tag_xref_table.c.wikibase_software_id == base_id
+                    )
+                ),
+            )
+        ),
+    )
+
+    return update_software_tags_query
+
+
+def get_select_software_query(id_list: list[int]) -> Select[WikibaseSoftwareModel]:
+    """Select WikibaseSoftwareModel in ID list"""
+
+    software_query = select(WikibaseSoftwareModel).where(
+        WikibaseSoftwareModel.id.in_(id_list)
+    )
+
+    return software_query
+
+
+def get_update_software_version_query(base_id: int, additional_id: int) -> Update:
+    """Update Software Version from Additional ID to Base ID"""
+
+    update_software_version_query = (
+        update(WikibaseSoftwareVersionModel)
+        .where(WikibaseSoftwareVersionModel.software_id == additional_id)
+        .values(software_id=base_id)
+    )
+
+    return update_software_version_query
