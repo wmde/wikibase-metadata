6886918f0f34bdd8fa66b630274e72378e480497
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-13 03:10:49 +0100

    Test Fail

--- tests/test_mutation/test_merge_software.py
+++ tests/test_mutation/test_merge_software.py
@@ -23,3 +23,19 @@ async def test_merge_software_by_id_mutation():
     assert result.errors is None
     assert result.data is not None
     assert result.data.get("mergeSoftwareById")
+
+
+@pytest.mark.asyncio
+@pytest.mark.mutation
+@pytest.mark.dependency(
+    depends=["software-version-success"],
+    name="merge-software-by-id-fail",
+    scope="session",
+)
+async def test_merge_software_by_id_fail_mutation():
+    """Test Add Wikibase"""
+
+    result = await test_schema.execute(
+        MERGE_SOFTWARE_QUERY, variable_values={"baseId": 1, "additionalId": 4}
+    )
+    assert result.errors is not None
