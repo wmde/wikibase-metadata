a7c0bb6680754549507af5b32bb9df308d3b8ce1
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-24 16:52:44 +0100

    Limit Instances

--- schedule/scheduler.py
+++ schedule/scheduler.py
@@ -21,27 +21,43 @@ from fetch_data import (
 scheduler = AsyncIOScheduler()
 
 scheduler.add_job(
-    update_out_of_date_connectivity_observations, CronTrigger(day_of_week=0, hour=0)
+    update_out_of_date_connectivity_observations,
+    CronTrigger(day_of_week=0, hour=0),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_log_first_observations, CronTrigger(day_of_week=0, hour=1)
+    update_out_of_date_log_first_observations,
+    CronTrigger(day_of_week=0, hour=1),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_log_last_observations, CronTrigger(day_of_week=0, hour=2)
+    update_out_of_date_log_last_observations,
+    CronTrigger(day_of_week=0, hour=2),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_property_observations, CronTrigger(day_of_week=0, hour=3)
+    update_out_of_date_property_observations,
+    CronTrigger(day_of_week=0, hour=3),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_quantity_observations, CronTrigger(day_of_week=0, hour=4)
+    update_out_of_date_quantity_observations,
+    CronTrigger(day_of_week=0, hour=4),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_software_observations, CronTrigger(day_of_week=0, hour=5)
+    update_out_of_date_software_observations,
+    CronTrigger(day_of_week=0, hour=5),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_stats_observations, CronTrigger(day_of_week=0, hour=6)
+    update_out_of_date_stats_observations,
+    CronTrigger(day_of_week=0, hour=6),
+    max_instances=1,
 )
 scheduler.add_job(
-    update_out_of_date_user_observations, CronTrigger(day_of_week=0, hour=7)
+    update_out_of_date_user_observations,
+    CronTrigger(day_of_week=0, hour=7),
+    max_instances=1,
 )
-scheduler.add_job(update_software_data, IntervalTrigger(hours=2))
+scheduler.add_job(update_software_data, IntervalTrigger(hours=2), max_instances=1)
