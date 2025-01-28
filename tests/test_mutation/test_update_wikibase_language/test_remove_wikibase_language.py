2519661f933b826af557b4641a7a8942693373c9
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-28 14:55:37 +0100

    Remove II

--- tests/test_mutation/test_update_wikibase_language/test_remove_wikibase_language.py
+++ tests/test_mutation/test_update_wikibase_language/test_remove_wikibase_language.py
@@ -55,3 +55,60 @@ async def test_remove_wikibase_language_one():
         remove_result.errors[0].message
         == "Cannot Remove Primary Language; Please Update First"
     )
+
+
+@pytest.mark.asyncio
+@pytest.mark.mutation
+@pytest.mark.dependency(
+    name="remove-wikibase-language-2",
+    depends=["add-wikibase-language-2"],
+    scope="session",
+)
+async def test_remove_wikibase_language_two():
+    """Add Wikibase Language"""
+
+    before_removing_result = await test_schema.execute(
+        WIKIBASE_LANGUAGES_QUERY, variable_values={"wikibaseId": 1}
+    )
+    assert before_removing_result.errors is None
+    assert before_removing_result.data is not None
+    assert_layered_property_value(
+        before_removing_result.data, ["wikibase", "id"], expected_value="1"
+    )
+    assert_layered_property_value(
+        before_removing_result.data,
+        ["wikibase", "languages", "primary"],
+        expected_value="French",
+    )
+    assert_layered_property_value(
+        before_removing_result.data,
+        ["wikibase", "languages", "additional"],
+        expected_value=["Albanian", "Babylonian", "Cymru", "Deutsch", "English"],
+    )
+
+    remove_result = await test_schema.execute(
+        REMOVE_WIKIBASE_LANGUAGE_QUERY,
+        variable_values={"wikibaseId": 1, "language": "English"},
+    )
+    assert remove_result.errors is None
+    assert remove_result.data is not None
+    assert remove_result.data["removeWikibaseLanguage"] is True
+
+    after_removing_result = await test_schema.execute(
+        WIKIBASE_LANGUAGES_QUERY, variable_values={"wikibaseId": 1}
+    )
+    assert after_removing_result.errors is None
+    assert after_removing_result.data is not None
+    assert_layered_property_value(
+        after_removing_result.data, ["wikibase", "id"], expected_value="1"
+    )
+    assert_layered_property_value(
+        after_removing_result.data,
+        ["wikibase", "languages", "primary"],
+        expected_value="French",
+    )
+    assert_layered_property_value(
+        after_removing_result.data,
+        ["wikibase", "languages", "additional"],
+        expected_value=["Albanian", "Babylonian", "Cymru", "Deutsch"],
+    )
