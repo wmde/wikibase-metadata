5615e5b6f8b245482ce940c3228ce1ee4ac4661b
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-28 14:30:18 +0100

    Test Add

---
+++ tests/test_mutation/test_update_wikibase_language.py
@@ -0,0 +1,75 @@
+import pytest
+
+from tests.test_schema import test_schema
+from tests.utils.assert_property_value import assert_layered_property_value
+
+WIKIBASE_LANGUAGES_QUERY = """
+query MyQuery($wikibaseId: Int!) {
+  wikibase(wikibaseId: $wikibaseId) {
+    id
+    languages {
+      primary
+      additional
+    }
+  }
+}"""
+
+ADD_WIKIBASE_LANGUAGE_QUERY = """
+mutation MyMutation($language: String!, $wikibaseId: Int!) {
+  addWikibaseLanguage(language: $language, wikibaseId: $wikibaseId)
+}"""
+
+
+@pytest.mark.asyncio
+@pytest.mark.mutation
+@pytest.mark.dependency(
+    name="add-wikibase-language", depends=["add-wikibase"], scope="session"
+)
+async def test_add_wikibase_language():
+    """Add Wikibase Language"""
+
+    before_adding_result = await test_schema.execute(
+        WIKIBASE_LANGUAGES_QUERY, variable_values={"wikibaseId": 1}
+    )
+    assert before_adding_result.errors is None
+    assert before_adding_result.data is not None
+    assert_layered_property_value(
+        before_adding_result.data, ["wikibase", "id"], expected_value="1"
+    )
+    assert_layered_property_value(
+        before_adding_result.data,
+        ["wikibase", "languages", "primary"],
+        expected_value=None,
+    )
+    assert_layered_property_value(
+        before_adding_result.data,
+        ["wikibase", "languages", "additional"],
+        expected_value=[],
+    )
+
+    add_result = await test_schema.execute(
+        ADD_WIKIBASE_LANGUAGE_QUERY,
+        variable_values={"wikibaseId": 1, "language": "French"},
+    )
+    assert add_result.errors is None
+    assert add_result.data is not None
+    assert add_result.data["addWikibaseLanguage"] is True
+
+    after_adding_result = await test_schema.execute(
+        WIKIBASE_LANGUAGES_QUERY, variable_values={"wikibaseId": 1}
+    )
+    assert after_adding_result.errors is None
+    assert after_adding_result.data is not None
+    assert_layered_property_value(
+        after_adding_result.data, ["wikibase", "id"], expected_value="1"
+    )
+    assert_layered_property_value(
+        after_adding_result.data,
+        ["wikibase", "languages", "primary"],
+        expected_value="French",
+    )
+    assert_layered_property_value(
+        after_adding_result.data,
+        ["wikibase", "languages", "additional"],
+        expected_value=[],
+    )
