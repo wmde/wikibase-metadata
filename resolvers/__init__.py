e444a3dd8f2bff26438a789f6b757c1fa720543b
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-31 13:45:27 +0100

    Init

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -9,6 +9,7 @@ from resolvers.get_aggregate_quantity import get_aggregate_quantity
 from resolvers.get_aggregate_software_version import get_aggregate_version
 from resolvers.get_aggregate_statistics import get_aggregate_statistics
 from resolvers.get_aggregate_users import get_aggregate_users
+from resolvers.get_language_list import get_language_list
 from resolvers.get_software_list import get_software_list
 from resolvers.get_wikibase import get_wikibase
 from resolvers.get_wikibase_list import get_wikibase_list
