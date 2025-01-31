5e588aca1bbd9b38bc79b4314d2ca63e6eb24c0a
Ricki Jay <ricki.jay@wikimedia.de>
2025-01-31 13:50:09 +0100

    Consistency II

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -2,6 +2,7 @@
 
 from resolvers.add_wikibase import add_wikibase
 from resolvers.get_aggregate_created import get_aggregate_created
+from resolvers.get_aggregate_languages import get_language_list
 from resolvers.get_aggregate_property_popularity import (
     get_aggregate_property_popularity,
 )
@@ -9,7 +10,6 @@ from resolvers.get_aggregate_quantity import get_aggregate_quantity
 from resolvers.get_aggregate_software_version import get_aggregate_version
 from resolvers.get_aggregate_statistics import get_aggregate_statistics
 from resolvers.get_aggregate_users import get_aggregate_users
-from resolvers.get_language_list import get_language_list
 from resolvers.get_software_list import get_software_list
 from resolvers.get_wikibase import get_wikibase
 from resolvers.get_wikibase_list import get_wikibase_list
