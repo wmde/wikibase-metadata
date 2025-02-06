3b78df9756fe66f3e7b17395aae4a20bf9e6c8a6
Ricki Jay <ricki.jay@wikimedia.de>
2025-02-06 15:11:20 +0100

    Does Not Work

--- schedule/scheduler.py
+++ schedule/scheduler.py
@@ -23,46 +23,30 @@ from fetch_data import (
 scheduler = AsyncIOScheduler()
 
 scheduler.add_job(
-    update_out_of_date_connectivity_observations,
-    CronTrigger(day_of_week=0, hour=0),
-    max_instances=1,
+    update_out_of_date_connectivity_observations, CronTrigger(day_of_week=0, hour=0)
 )
 scheduler.add_job(
-    update_out_of_date_log_first_observations,
-    CronTrigger(day_of_week=0, hour=1),
-    max_instances=1,
+    update_out_of_date_log_first_observations, CronTrigger(day_of_week=0, hour=1)
 )
 scheduler.add_job(
-    update_out_of_date_log_last_observations,
-    CronTrigger(day_of_week=0, hour=2),
-    max_instances=1,
+    update_out_of_date_log_last_observations, CronTrigger(day_of_week=0, hour=2)
 )
 scheduler.add_job(
-    update_out_of_date_property_observations,
-    CronTrigger(day_of_week=0, hour=3),
-    max_instances=1,
+    update_out_of_date_property_observations, CronTrigger(day_of_week=0, hour=3)
 )
 scheduler.add_job(
-    update_out_of_date_quantity_observations,
-    CronTrigger(day_of_week=0, hour=4),
-    max_instances=1,
+    update_out_of_date_quantity_observations, CronTrigger(day_of_week=0, hour=4)
 )
 scheduler.add_job(
-    update_out_of_date_software_observations,
-    CronTrigger(day_of_week=0, hour=5),
-    max_instances=1,
+    update_out_of_date_software_observations, CronTrigger(day_of_week=0, hour=5)
 )
 scheduler.add_job(
-    update_out_of_date_stats_observations,
-    CronTrigger(day_of_week=0, hour=6),
-    max_instances=1,
+    update_out_of_date_stats_observations, CronTrigger(day_of_week=0, hour=6)
 )
 scheduler.add_job(
-    update_out_of_date_user_observations,
-    CronTrigger(day_of_week=0, hour=7),
-    max_instances=1,
+    update_out_of_date_user_observations, CronTrigger(day_of_week=0, hour=7)
 )
-scheduler.add_job(update_software_data, IntervalTrigger(hours=2), max_instances=1)
+scheduler.add_job(update_software_data, IntervalTrigger(hours=2))
 
 
 def hello_world():
@@ -70,4 +54,4 @@ def hello_world():
     print(f"Hello World - {os.getpid()} - {datetime.now()}")
 
 
-scheduler.add_job(hello_world, IntervalTrigger(seconds=5), max_instances=1)
+scheduler.add_job(hello_world, IntervalTrigger(seconds=5))
