fea059da9c8c9b8e6e7e2ad589bd4324414782a8
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-06-05 00:53:42 +0200

    I/O

--- model/strawberry/output/wikibase_url_set.py
+++ model/strawberry/output/wikibase_url_set.py
@@ -11,17 +11,30 @@ class WikibaseURLSetStrawberryModel:
     """Wikibase URL Set"""
 
     base_url: str = strawberry.field(description="Base URL")
-    action_api: Optional[str] = strawberry.field(description="Action API URL")
-    index_api: Optional[str] = strawberry.field(description="Index API URL")
-    sparql_url: Optional[str] = strawberry.field(description="SPARQL URL")
+    article_path: str = strawberry.field(description="Article Path `/wiki`")
+    script_path: str = strawberry.field(description="Script Path `/w`")
     sparql_endpoint_url: Optional[str] = strawberry.field(
         description="SPARQL Endpoint URL"
     )
+    sparql_frontend_url: Optional[str] = strawberry.field(
+        description="SPARQL Frontend URL"
+    )
+
+    action_api: Optional[str] = strawberry.field(
+        description="Action API URL", deprecation_reason="Use scriptPath"
+    )
+    index_api: Optional[str] = strawberry.field(
+        description="Index API URL", deprecation_reason="Use scriptPath"
+    )
+    sparql_url: Optional[str] = strawberry.field(
+        description="SPARQL URL",
+        deprecation_reason="Renamed to sparqlFrontendURL for clarity",
+    )
     special_statistics_url: Optional[str] = strawberry.field(
-        description="Special:Statistics URL"
+        description="Special:Statistics URL", deprecation_reason="Use articlePath"
     )
     special_version_url: Optional[str] = strawberry.field(
-        description="Special:Version URL"
+        description="Special:Version URL", deprecation_reason="Use articlePath"
     )
 
     @classmethod
@@ -30,6 +43,18 @@ class WikibaseURLSetStrawberryModel:
 
         return cls(
             base_url=model.url.url,
+            article_path=model.article_path.url,
+            script_path=model.script_path.url,
+            sparql_endpoint_url=(
+                model.sparql_endpoint_url.url
+                if model.sparql_endpoint_url is not None
+                else None
+            ),
+            sparql_frontend_url=(
+                model.sparql_frontend_url.url
+                if model.sparql_frontend_url is not None
+                else None
+            ),
             action_api=(
                 model.action_api_url.url if model.action_api_url is not None else None
             ),
@@ -37,13 +62,8 @@ class WikibaseURLSetStrawberryModel:
                 model.index_api_url.url if model.index_api_url is not None else None
             ),
             sparql_url=(
-                model.sparql_query_url.url
-                if model.sparql_query_url is not None
-                else None
-            ),
-            sparql_endpoint_url=(
-                model.sparql_endpoint_url.url
-                if model.sparql_endpoint_url is not None
+                model.sparql_frontend_url.url
+                if model.sparql_frontend_url is not None
                 else None
             ),
             special_statistics_url=(
