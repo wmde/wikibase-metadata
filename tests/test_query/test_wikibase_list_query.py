c8c8fab226cf8743b048603e697779490608adbe
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-06-05 13:11:04 +0200

    URLS

--- tests/test_query/test_wikibase_list_query.py
+++ tests/test_query/test_wikibase_list_query.py
@@ -125,9 +125,9 @@ async def test_wikibase_list_query():
 
     for url_name, url in [
         ("baseUrl", "example.com"),
-        ("actionApi", "example.com/w/api.php"),
+        ("actionApi", "https://example.com/w/api.php"),
         ("indexApi", "example.com/w/index.php"),
-        ("sparqlEndpointUrl", "query.example.com/sparql"),
+        ("sparqlEndpointUrl", "https://query.example.com/sparql"),
         ("sparqlUrl", "query.example.com"),
         ("specialStatisticsUrl", "example.com/wiki/Special:Statistics"),
         ("specialVersionUrl", "example.com/wiki/Special:Version"),
