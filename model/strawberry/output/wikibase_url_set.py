60179fe660fa7ca6118c6422cf4a3d526820f905
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-06-05 15:36:27 +0200

    Optional

--- model/strawberry/output/wikibase_url_set.py
+++ model/strawberry/output/wikibase_url_set.py
@@ -11,8 +11,8 @@ class WikibaseURLSetStrawberryModel:
     """Wikibase URL Set"""
 
     base_url: str = strawberry.field(description="Base URL")
-    article_path: str = strawberry.field(description="Article Path `/wiki`")
-    script_path: str = strawberry.field(description="Script Path `/w`")
+    article_path: Optional[str] = strawberry.field(description="Article Path `/wiki`")
+    script_path: Optional[str] = strawberry.field(description="Script Path `/w`")
     sparql_endpoint_url: Optional[str] = strawberry.field(
         description="SPARQL Endpoint URL"
     )
@@ -43,8 +43,12 @@ class WikibaseURLSetStrawberryModel:
 
         return cls(
             base_url=model.url.url,
-            article_path=model.article_path.url,
-            script_path=model.script_path.url,
+            article_path=(
+                model.article_path.url if model.article_path is not None else None
+            ),
+            script_path=(
+                model.script_path.url if model.script_path is not None else None
+            ),
             sparql_endpoint_url=(
                 model.sparql_endpoint_url.url
                 if model.sparql_endpoint_url is not None
