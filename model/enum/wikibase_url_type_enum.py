9312ba36e56d64a9de75c4d495e868b47a7b62f6
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-06-05 14:50:31 +0200

    Do Include Deprecated

--- model/enum/wikibase_url_type_enum.py
+++ model/enum/wikibase_url_type_enum.py
@@ -7,7 +7,26 @@ class WikibaseURLType(enum.Enum):
     """Wikibase URL Type"""
 
     BASE_URL = 1
-    ARTICLE_PATH = 2
-    SCRIPT_PATH = 3
+
+    ACTION_QUERY_URL = 2
+    """Deprecated"""
+
+    ARTICLE_PATH = 8
+
+    INDEX_QUERY_URL = 3
+    """Deprecated"""
+
+    SCRIPT_PATH = 9
+
     SPARQL_ENDPOINT_URL = 4
-    SPARQL_FRONTEND_URL = 5
+
+    SPARQL_FRONTEND_URL = 10
+
+    SPARQL_QUERY_URL = 5
+    """Deprecated"""
+
+    SPECIAL_STATISTICS_URL = 6
+    """Deprecated"""
+
+    SPECIAL_VERSION_URL = 7
+    """Deprecated"""
