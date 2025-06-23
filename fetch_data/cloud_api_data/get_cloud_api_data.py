e004b3595c31ff322fff21fa2b7f799f6fbf25dd
Robert Timm <mail@rtti.de>
2025-06-23 08:56:02 +0000

    docs: put Tom's comment in code

--- fetch_data/cloud_api_data/get_cloud_api_data.py
+++ fetch_data/cloud_api_data/get_cloud_api_data.py
@@ -12,8 +12,14 @@ class WikibaseCloudInstance:
 
     id: int
     description: str | None
+
+    # domain is PUNYcode, domain_decoded is unicode.
+    #  e.g. 
+    #   "domain":"xn--brgerspitalzinshaus-59b.wikibase.cloud"
+    #   "domain_decoded":"b\u00fcrgerspitalzinshaus.wikibase.cloud"
     domain: str
     domain_decoded: str
+
     sitename: str | None
 
 
