77ccd6af14377250d28a22a113546259384c52ab
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-06-24 14:48:58 +0200

    Split

---
+++ tests/test_upsert_cloud_instances/constant.py
@@ -0,0 +1,3 @@
+"""Constants"""
+
+DATA_DIRECTORY = "tests/test_upsert_cloud_instances/data"
--- tests/test_upsert_cloud_instances/test_upsert_cloud_instances.py
+++ tests/test_upsert_cloud_instances/test_upsert_cloud_instances.py
@@ -1,75 +1,15 @@
-"""Test create_special_statistics_observation"""
+"""Test Upsert Cloud Instances"""
 
 import os
 import pytest
 from sqlalchemy import select
 
 from data.database_connection import get_async_session
-from fetch_data import fetch_cloud_instances, update_cloud_instances
+from fetch_data import update_cloud_instances
 from model.database import WikibaseModel, WikibaseURLModel
 from model.enum import WikibaseType
-from tests.test_schema import test_schema
-from tests.utils import get_mock_context, MockResponse
-
-
-DATA_DIRECTORY = "tests/test_upsert_cloud_instances/data"
-
-
-@pytest.mark.asyncio
-async def test_fetch_cloud_instances(mocker):
-    """
-    test fetching a long list of cloud instances and
-    examine some examples if their values are correctly parsed
-    """
-
-    with open(
-        os.path.join(DATA_DIRECTORY, "instances-all.json"), "rb"
-    ) as instances_json:
-
-        mocker.patch(
-            "requests.get",
-            side_effect=[MockResponse("", 200, instances_json.read())],
-        )
-
-        instances = await fetch_cloud_instances()
-
-        assert instances
-        assert len(instances) == 8
-
-        assert instances[0].id == 167
-        assert instances[0].sitename == "Doelgericht Digitaal Transformeren"
-        assert instances[0].domain == "osloddt.wikibase.cloud"
-        assert instances[0].domain_decoded == "osloddt.wikibase.cloud"
-        assert instances[0].description is None
-
-        assert instances[7].id == 326
-        assert instances[7].sitename == "MetaBase"
-        assert instances[7].domain == "metabase.wikibase.cloud"
-        assert instances[7].domain_decoded == "metabase.wikibase.cloud"
-        assert instances[7].description is None
-
-
-@pytest.mark.asyncio
-async def test_fetch_cloud_instances_broken_response(mocker):
-    """
-    test fetching a list with one broken cloud instance and
-    examine the remaining correct instance
-    """
-
-    with open(
-        os.path.join(DATA_DIRECTORY, "instances-broken.json"), "rb"
-    ) as instances_json:
-
-        mocker.patch(
-            "requests.get",
-            side_effect=[MockResponse("", 200, instances_json.read())],
-        )
-
-        instances = await fetch_cloud_instances()
-
-        assert instances
-        assert len(instances) == 1
-        assert instances[0].id == 32
+from tests.test_upsert_cloud_instances.constant import DATA_DIRECTORY
+from tests.utils import MockResponse
 
 
 @pytest.mark.dependency(name="insert-cloud-instances", scope="session")
@@ -239,44 +179,3 @@ async def test_transform_to_cloud_instance(mocker):
                 found.sparql_endpoint_url.url
                 == "https://tcdict.wikibase.cloud/query/sparql"
             )
-
-
-WIKIBASE_LIST_QUERY = """
-query MyQuery {
-  wikibaseList(pageNumber: 1, pageSize: 10000) {
-    data {
-      id
-      urls {
-        baseUrl
-      }
-    }
-  }
-}
-"""
-
-
-@pytest.mark.dependency(
-    name="query-cloud-instances", depends=["transform-cloud-instances"], scope="session"
-)
-@pytest.mark.asyncio
-async def test_query_cloud_instance():
-    """
-    test whether querying the wikibase list via graphql returns a cloud instance
-    """
-    result = await test_schema.execute(
-        WIKIBASE_LIST_QUERY, context_value=get_mock_context("test-auth-token")
-    )
-    assert result.errors is None
-    assert result.data is not None
-    data = result.data
-    assert "wikibaseList" in data
-    wikibase_list = data["wikibaseList"]
-    assert "data" in wikibase_list
-    wikibase_list_data = wikibase_list["data"]
-
-    found = [
-        wikibase
-        for wikibase in wikibase_list_data
-        if wikibase["urls"]["baseUrl"] == "https://tcdict.wikibase.cloud"
-    ]
-    assert len(found) == 1
