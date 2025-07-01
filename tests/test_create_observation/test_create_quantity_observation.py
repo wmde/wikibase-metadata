bd0da344d230161539ea2e3805a26bba6358ef3d
Robert Timm <mail@rtti.de>
2025-07-01 15:41:10 +0000

    test: fetch

--- tests/test_create_observation/test_create_quantity_observation.py
+++ tests/test_create_observation/test_create_quantity_observation.py
@@ -22,6 +22,10 @@ async def test_create_quantity_observation_success(mocker):
             {"results": {"bindings": [{"count": {"value": 2}}]}},  # Items
             {"results": {"bindings": [{"count": {"value": 4}}]}},  # Lexemes
             {"results": {"bindings": [{"count": {"value": 8}}]}},  # Triples
+            {"results": {"bindings": [{"count": {"value": 8}}]}},  # External Identifier Properties
+            {"results": {"bindings": [{"count": {"value": 8}}]}},  # External Identifier Statements
+            {"results": {"bindings": [{"count": {"value": 8}}]}},  # URL Properties
+            {"results": {"bindings": [{"count": {"value": 8}}]}},  # URL Statements
         ],
     )
     success = await create_quantity_observation(1)
