ff797d7ef7934a8ed346d5eb4f31a7faf61a2bea
Robert Timm <mail@rtti.de>
2025-07-28 14:53:59 +0200

    Revert "chore: remove unused variable"
    This reverts commit 1f5b41ffb28a7034750d055bb01946537970dd67.

--- tests/test_create_observation/test_create_recent_changes_observation/test_create_recent_changes.py
+++ tests/test_create_observation/test_create_recent_changes_observation/test_create_recent_changes.py
@@ -16,6 +16,7 @@ from model.database import WikibaseModel, WikibaseRecentChangesObservationModel
 @pytest.mark.asyncio
 async def test_create_recent_changes_empty():
     """Test empty list scenario"""
+    wikibase = WikibaseModel(wikibase_name="Test", base_url="http://test.com")
     observation = WikibaseRecentChangesObservationModel()
     result = await create_recent_changes(wikibase, [], observation)
     assert result.change_count == 0
@@ -27,6 +28,7 @@ async def test_create_recent_changes_empty():
 @pytest.mark.asyncio
 async def test_create_recent_changes_counts():
     """Test that user and change counts are calculated correctly"""
+    wikibase = WikibaseModel(wikibase_name="Test", base_url="http://test.com")
     observation = WikibaseRecentChangesObservationModel()
     records = [
         WikibaseRecentChangeRecord(
