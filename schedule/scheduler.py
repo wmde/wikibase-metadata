d9ef432f1debca51d30ce8cfd766f99cda689ff5
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-08-05 18:14:46 +0200

    Infrequent

--- schedule/scheduler.py
+++ schedule/scheduler.py
@@ -60,8 +60,7 @@ scheduler.add_job(
 )
 scheduler.add_job(
     update_out_of_date_stats_observations,
-    # CronTrigger(day_of_week=0, hour=6),
-    IntervalTrigger(minutes=5),
+    CronTrigger(day_of_week=0, hour=6),
 )
 scheduler.add_job(
     update_out_of_date_user_observations,
