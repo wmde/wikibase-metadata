1e71905a97e1a0e0cc40f6c426850101ce72c685
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-08-11 15:18:49 +0200

    FULL_URL_PATTERN

--- resolvers/util/clean_wikibase_url.py
+++ resolvers/util/clean_wikibase_url.py
@@ -5,14 +5,21 @@ import re
 from model.enum import WikibaseURLType
 
 
+FULL_URL_PATTERN = re.compile(r"https?://[A-z0-9\-_.\?=]+", re.IGNORECASE)
+
+
 def clean_up_url(url: str, url_type: WikibaseURLType) -> str:
     """Clean URL"""
+
+    stripped_url = url.strip()
 
     if url_type in [
         WikibaseURLType.BASE_URL,
         WikibaseURLType.SPARQL_ENDPOINT_URL,
         WikibaseURLType.SPARQL_FRONTEND_URL,
     ]:
-        assert re.match(r"https?://[A-z0-9\-_.\?=]+", url)
+        assert FULL_URL_PATTERN.match(stripped_url)
+    else:
+        assert not FULL_URL_PATTERN.match(stripped_url)
 
-    return url.strip()
+    return stripped_url
