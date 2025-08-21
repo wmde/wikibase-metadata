b1096917e7180fa469394a888664410a446870c6
Robert Timm <mail@rtti.de>
2025-08-21 15:35:15 +0000

    feat: retry sparql on 429 rate limit

--- fetch_data/sparql_data/pull_wikidata.py
+++ fetch_data/sparql_data/pull_wikidata.py
@@ -8,24 +8,55 @@ import sys
 from SPARQLWrapper import QueryResult, SPARQLWrapper, JSON
 from SPARQLWrapper.SPARQLExceptions import SPARQLWrapperException
 
-from logger import logger
+from logger.get_logger import logger
+
+import time
+from urllib.error import HTTPError
+
+
+async def get_sparql_results(
+    endpoint_url: str,
+    query: str,
+    query_name: str,
+    query_timeout: int = 0,
+    max_retries: int = 5,
+    backup_time_init: int = 8,
+) -> dict:
+    """Get SPARQL Data from a Wikibase async"""
+    backup_time = backup_time_init
+
+    retires = 0
+    while True:
+        try:
+            logger.debug(f"SparQL Query '{query_name}' on '{endpoint_url}': '{query}'")
+            return await asyncio.to_thread(
+                _get_results,
+                endpoint_url=endpoint_url,
+                query=query,
+                query_name=query_name,
+                timeout=query_timeout,
+            )
+
+        except HTTPError as exc:
+            if exc.code != 429 or retires >= max_retries:
+                logger.warning(
+                    f"SPARQLError: {exc}",
+                    # exc_info=True,
+                    # stack_info=True,
+                )
+                raise exc
+
+            else:
+                logger.warning(
+                    f"SparQL Query '{query_name}' on '{endpoint_url}' received 429, sleeping for {backup_time} seconds"
+                )
+                time.sleep(backup_time)
+                backup_time = backup_time * 2
+                retires += 1
 
 
 class SPARQLResponseMalformed(SPARQLWrapperException):
     """Response Unexpected Format"""
-
-
-async def get_sparql_results(
-    endpoint_url: str, query: str, query_name: str, timeout: int = 60
-) -> dict:
-    """Get SPARQL Data from Wikidata ASYNC"""
-    return await asyncio.to_thread(
-        _get_results,
-        endpoint_url=endpoint_url,
-        query=query,
-        query_name=query_name,
-        timeout=timeout,
-    )
 
 
 # retrieve results from a given endpoint given a distinct SPARQL query
