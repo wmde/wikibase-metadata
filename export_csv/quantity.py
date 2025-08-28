d242a22968f81b0d467713140bd5431498d12564
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-08-28 10:50:03 +0200

    Quantity

--- export_csv/quantity.py
+++ export_csv/quantity.py
@@ -4,10 +4,11 @@ import os
 import uuid
 from fastapi import BackgroundTasks
 from fastapi.responses import StreamingResponse
-from sqlalchemy import select
+from sqlalchemy import and_, func, or_, select
 
 from export_csv.util import read_sql_query
-from model.database.wikibase_model import WikibaseModel
+from model.database import WikibaseModel, WikibaseQuantityObservationModel
+from model.enum import WikibaseType
 
 
 CHUNK_SIZE = 1024 * 1024
@@ -16,9 +17,48 @@ CHUNK_SIZE = 1024 * 1024
 async def export_quantity_csv(background_tasks: BackgroundTasks):
     """Quantity CSV"""
 
-    select_query = select(WikibaseModel)
+    filtered_subquery = (
+        select(WikibaseModel)
+        .where(
+            or_(
+                # pylint: disable-next=singleton-comparison
+                WikibaseModel.wikibase_type == None,
+                and_(
+                    WikibaseModel.wikibase_type != WikibaseType.CLOUD,
+                    WikibaseModel.wikibase_type != WikibaseType.TEST,
+                ),
+            )
+        )
+        .subquery()
+    )
+    rank_subquery = (
+        select(
+            WikibaseQuantityObservationModel.id,
+            # pylint: disable-next=not-callable
+            func.rank()
+            .over(
+                partition_by=WikibaseQuantityObservationModel.wikibase_id,
+                order_by=WikibaseQuantityObservationModel.observation_date.desc(),
+            )
+            .label("rank"),
+        )
+        .join(
+            filtered_subquery,
+            onclause=WikibaseQuantityObservationModel.wikibase_id
+            == filtered_subquery.c.id,
+        )
+        .where((WikibaseQuantityObservationModel.returned_data))
+        .subquery()
+    )
+    query = select(WikibaseQuantityObservationModel).join(
+        rank_subquery,
+        onclause=and_(
+            WikibaseQuantityObservationModel.id == rank_subquery.c.id,
+            rank_subquery.c.rank == 1,
+        ),
+    )
 
-    df = await read_sql_query(select_query)
+    df = await read_sql_query(query)
 
     filename = f"{uuid.uuid4()}.csv"
     df.to_csv(filename)
