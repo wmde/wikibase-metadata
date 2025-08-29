beb964a53087dd49edbc67b0f4648c8e6340e9c7
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-08-29 10:53:43 +0200

    index_col

--- export_csv/util.py
+++ export_csv/util.py
@@ -1,18 +1,21 @@
 """Utilities"""
 
+from typing import Optional
 import pandas
 from sqlalchemy import Connection, Select
 
 from data.database_connection import async_engine
 
 
-def _read_sql_query(con: Connection, stmt: Select):
-    return pandas.read_sql_query(stmt, con)
+def _read_sql_query(con: Connection, stmt: Select, index_col: Optional[str] = None):
+    return pandas.read_sql_query(stmt, con, index_col=index_col)
 
 
-async def read_sql_query(stmt: Select) -> pandas.DataFrame:
+async def read_sql_query(
+    stmt: Select, index_col: Optional[str] = None
+) -> pandas.DataFrame:
     """Read SQL to DataFrame"""
 
     async with async_engine.begin() as conn:
-        df = await conn.run_sync(_read_sql_query, stmt)
+        df = await conn.run_sync(_read_sql_query, stmt, index_col=index_col)
         return df
