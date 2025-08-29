af2a9b06365212ed9966751b41902dbdb9ac498d
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-08-29 12:20:37 +0200

    Util

--- export_csv/metric.py
+++ export_csv/metric.py
@@ -1,12 +1,9 @@
 """Quantity CSV"""
 
-import os
-import uuid
 from fastapi import BackgroundTasks
-from fastapi.responses import StreamingResponse
 from sqlalchemy import and_, func, or_, select
 
-from export_csv.util import read_sql_query
+from export_csv.util import export_csv
 from model.database import (
     WikibaseModel,
     WikibaseQuantityObservationModel,
@@ -18,29 +15,17 @@ from model.database import (
 from model.enum import WikibaseType
 
 
-CHUNK_SIZE = 1024 * 1024
-
-
 async def export_metric_csv(background_tasks: BackgroundTasks):
     """CSV with Requested Metrics"""
 
     query = get_metrics_query()
 
-    df = await read_sql_query(query, index_col="wikibase_id")
-
-    filename = f"{uuid.uuid4()}.csv"
-    df.to_csv(filename)
-    del df
-
-    def iterfile():
-        with open(filename, "rb") as f:
-            while chunk := f.read(CHUNK_SIZE):
-                yield chunk
-
-    background_tasks.add_task(os.remove, filename)
-
-    headers = {"Content-Disposition": 'attachment; filename="metrics.csv"'}
-    return StreamingResponse(iterfile(), headers=headers, media_type="text/csv")
+    return await export_csv(
+        query=query,
+        export_filename="metrics",
+        index_col="wikibase_id",
+        background_tasks=background_tasks,
+    )
 
 
 def get_metrics_query():
--- export_csv/util.py
+++ export_csv/util.py
@@ -1,6 +1,10 @@
 """Utilities"""
 
+import os
 from typing import Optional
+import uuid
+from fastapi import BackgroundTasks
+from fastapi.responses import StreamingResponse
 import pandas
 from sqlalchemy import Connection, Select
 
@@ -19,3 +23,31 @@ async def read_sql_query(
     async with async_engine.begin() as conn:
         df = await conn.run_sync(_read_sql_query, stmt, index_col=index_col)
         return df
+
+
+CHUNK_SIZE = 1024 * 1024
+
+
+async def export_csv(
+    query: Select,
+    export_filename: str,
+    background_tasks: BackgroundTasks,
+    index_col: Optional[str] = None,
+):
+    """Export CSV"""
+
+    df = await read_sql_query(query, index_col=index_col)
+
+    filename = f"{uuid.uuid4()}.csv"
+    df.to_csv(filename)
+    del df
+
+    def iterfile():
+        with open(filename, "rb") as f:
+            while chunk := f.read(CHUNK_SIZE):
+                yield chunk
+
+    background_tasks.add_task(os.remove, filename)
+
+    headers = {"Content-Disposition": f'attachment; filename="{export_filename}.csv"'}
+    return StreamingResponse(iterfile(), headers=headers, media_type="text/csv")
