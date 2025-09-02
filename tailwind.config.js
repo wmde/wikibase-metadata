7db61e27a82f978a1563978924b2913d102a067e
Robert Timm <mail@rtti.de>
2025-09-02 12:47:38 +0000

    refactor: move frontend folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./frontend/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
