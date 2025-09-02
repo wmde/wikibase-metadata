f605c28416585930d6d44795aed970c6c16578e2
Robert Timm <mail@rtti.de>
2025-09-02 12:41:43 +0000

    feat: tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,29 +7,33 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-3xl font-semibold">{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+    <button
+      type="button"
+      class="rounded-md bg-indigo-600 px-4 py-2 text-white shadow hover:bg-indigo-500"
+      @click="count++"
+    >
+      count is {{ count }}
+    </button>
+    <p class="mt-4 text-sm text-gray-600 dark:text-gray-300">
+      Edit <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
-  <p>
+  <p class="mt-6">
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a class="underline" href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>,
+    the official Vue + Vite starter
   </p>
-  <p>
+  <p class="text-sm text-gray-600 dark:text-gray-300">
     Learn more about IDE Support for Vue in the
     <a
+      class="underline"
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    >Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
