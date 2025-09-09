03934f663bff1ffd7cd12c1f641b142c1962982c
Robert Timm <mail@rtti.de>
2025-09-09 11:03:47 +0000

    debug on finished observation

--- fetch_data/sparql_data/create_quantity_data_observation.py
+++ fetch_data/sparql_data/create_quantity_data_observation.py
@@ -43,6 +43,10 @@ async def create_quantity_observation(wikibase_id: int) -> bool:
         wikibase.quantity_observations.append(observation)
 
         await async_session.commit()
+        logger.debug(
+            "Quantity: Observation returned data: " + observation.returned_data,
+            extra={"wikibase": wikibase_id},
+        )
         return observation.returned_data
 
 
