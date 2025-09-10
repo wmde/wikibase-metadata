d4237adb35e7047a604d98e17de100c5ab038e37
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-09-10 12:00:26 +0200

    Create Obs

--- fetch_data/sparql_data/__init__.py
+++ fetch_data/sparql_data/__init__.py
@@ -3,6 +3,9 @@
 from fetch_data.sparql_data.create_connectivity_data_observation import (
     create_connectivity_observation,
 )
+from fetch_data.sparql_data.create_external_identifier_data_observation import (
+    create_external_identifier_observation,
+)
 from fetch_data.sparql_data.create_property_popularity_data_observation import (
     create_property_popularity_observation,
 )
--- fetch_data/sparql_data/create_quantity_data_observation.py
+++ fetch_data/sparql_data/create_quantity_data_observation.py
@@ -11,13 +11,9 @@ from fetch_data.sparql_data.pull_wikidata import (
     get_sparql_results,
 )
 from fetch_data.sparql_data.sparql_queries import (
-    COUNT_EXTERNAL_IDENTIFIER_PROPERTIES_QUERY,
-    COUNT_EXTERNAL_IDENTIFIER_STATEMENTS_QUERY,
     COUNT_ITEMS_QUERY,
     COUNT_LEXEMES_QUERY,
     COUNT_PROPERTIES_QUERY,
-    COUNT_URL_PROPERTIES_QUERY,
-    COUNT_URL_STATEMENTS_QUERY,
     COUNT_TRIPLES_QUERY,
 )
 from fetch_data.utils import get_wikibase_from_database
@@ -91,62 +87,6 @@ async def compile_quantity_observation(
         )
         observation.total_triples = int(
             triple_count_results["results"]["bindings"][0]["count"]["value"]
-        )
-
-        logger.info(
-            "Fetching External Identifier Properties Count",
-            extra={"wikibase": wikibase.id},
-        )
-        external_identifier_properties_count_results = await get_sparql_results(
-            wikibase.sparql_endpoint_url.url,
-            COUNT_EXTERNAL_IDENTIFIER_PROPERTIES_QUERY,
-            "COUNT_EXTERNAL_IDENTIFIER_PROPERTIES_QUERY",
-        )
-        observation.total_external_identifier_properties = int(
-            external_identifier_properties_count_results["results"]["bindings"][0][
-                "count"
-            ]["value"]
-        )
-
-        logger.info(
-            "Fetching External Identifier Statements Count",
-            extra={"wikibase": wikibase.id},
-        )
-        external_identifier_statements_count_results = await get_sparql_results(
-            wikibase.sparql_endpoint_url.url,
-            COUNT_EXTERNAL_IDENTIFIER_STATEMENTS_QUERY,
-            "COUNT_EXTERNAL_IDENTIFIER_STATEMENTS_QUERY",
-        )
-        observation.total_external_identifier_statements = int(
-            external_identifier_statements_count_results["results"]["bindings"][0][
-                "count"
-            ]["value"]
-        )
-
-        logger.info(
-            "Fetching URL Properties Count",
-            extra={"wikibase": wikibase.id},
-        )
-        url_properties_count_results = await get_sparql_results(
-            wikibase.sparql_endpoint_url.url,
-            COUNT_URL_PROPERTIES_QUERY,
-            "COUNT_URL_PROPERTIES_QUERY",
-        )
-        observation.total_url_properties = int(
-            url_properties_count_results["results"]["bindings"][0]["count"]["value"]
-        )
-
-        logger.info(
-            "Fetching URL Statements Count",
-            extra={"wikibase": wikibase.id},
-        )
-        url_statements_count_results = await get_sparql_results(
-            wikibase.sparql_endpoint_url.url,
-            COUNT_URL_STATEMENTS_QUERY,
-            "COUNT_URL_STATEMENTS_QUERY",
-        )
-        observation.total_url_statements = int(
-            url_statements_count_results["results"]["bindings"][0]["count"]["value"]
         )
 
         observation.returned_data = True
