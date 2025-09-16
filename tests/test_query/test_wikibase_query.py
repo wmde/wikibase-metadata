20d5e6bff592e64d7b56e57ed5137644c67dc8a1
Robert Timm <mail@rtti.de>
2025-09-16 07:09:06 +0000

    chore: allow read requests without auth token

--- tests/test_query/test_wikibase_query.py
+++ tests/test_query/test_wikibase_query.py
@@ -3,7 +3,7 @@
 import pytest
 
 from tests.test_schema import test_schema
-from tests.utils import MockRequest, get_mock_context
+from tests.utils import get_mock_context
 
 
 WIKIBASE_QUERY = """
@@ -13,60 +13,6 @@ query MyQuery($wikibaseId: Int!) {
     title
   }
 }"""
-
-
-@pytest.mark.asyncio
-@pytest.mark.query
-@pytest.mark.dependency(depends=["add-wikibase"], scope="session")
-async def test_wikibase_query_unauthorized():
-    """Test Query Wikibase Unauthorized"""
-
-    result = await test_schema.execute(
-        WIKIBASE_QUERY,
-        variable_values={"wikibaseId": 1},
-        context_value={"request": MockRequest(headers={})},
-    )
-
-    assert result.errors is not None
-    assert result.errors[0].message == "Authorization header missing"
-
-    result = await test_schema.execute(
-        WIKIBASE_QUERY,
-        variable_values={"wikibaseId": 1},
-        context_value={
-            "request": MockRequest(headers={"authorization": "wrong-header-value"})
-        },
-    )
-
-    assert result.errors is not None
-    assert result.errors[0].message == "Invalid authorization header, expected 'bearer'"
-
-    result = await test_schema.execute(
-        WIKIBASE_QUERY,
-        variable_values={"wikibaseId": 1},
-        context_value={
-            "request": MockRequest(
-                headers={"authorization": "bearer: wrong token with spaces"}
-            )
-        },
-    )
-
-    assert result.errors is not None
-    assert (
-        result.errors[0].message
-        == "Invalid authorization header, expected 'bearer <token>'"
-    )
-
-    result = await test_schema.execute(
-        WIKIBASE_QUERY,
-        variable_values={"wikibaseId": 1},
-        context_value={
-            "request": MockRequest(headers={"authorization": "bearer: wrong-token"})
-        },
-    )
-
-    assert result.errors is not None
-    assert result.errors[0].message == "Authorization Failed"
 
 
 @pytest.mark.asyncio
