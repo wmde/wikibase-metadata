c1c4abae4ee5532b0edfe07863fa955ee3c8762a
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-14 20:15:16 +0200

    Filters I

--- frontend/src/component/WikibaseTable.vue
+++ frontend/src/component/WikibaseTable.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import { WikibaseType } from '@/graphql/types'
 import { useWikiStore } from '@/stores/wikibase-page-store'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import LocaleNumber from './LocaleNumber.vue'
 
 const store = useWikiStore()
@@ -9,12 +10,29 @@ const error = computed(() => store.wikibasePage.errorState)
 const loading = computed(() => store.wikibasePage.loading)
 const count = computed(() => store.wikibasePage.data?.wikibaseList.meta.totalCount)
 const wikibases = computed(() => store.wikibasePage.data?.wikibaseList.data)
+
+const excludedTypes = ref<WikibaseType[]>(store.wikibaseFilter.wikibaseType?.exclude ?? [])
+const typeOptions = [WikibaseType.Cloud, WikibaseType.Suite, WikibaseType.Other, WikibaseType.Test]
+watch(excludedTypes, () => store.excludeWikibaseTypes(excludedTypes.value))
 </script>
 
 <template>
-	<p v-if="error">Error</p>
-	<p v-if="loading">Loading</p>
-	<p>Count: <LocaleNumber :stat="count" /></p>
+	<v-container>
+		<p v-if="error">Error</p>
+		<p v-if="loading">Loading</p>
+		<p>Count: <LocaleNumber :stat="count" /></p>
+	</v-container>
+
+	<v-container>
+		<v-select
+			label="Exclude Wikibase Types"
+			v-model="excludedTypes"
+			:items="typeOptions"
+			multiple
+			chips
+		/>
+	</v-container>
+
 	<v-table v-if="wikibases" striped="even" :height="800" fixed-header fixed-footer>
 		<thead>
 			<tr>
@@ -28,7 +46,9 @@ const wikibases = computed(() => store.wikibasePage.data?.wikibaseList.data)
 		</thead>
 		<tbody>
 			<tr v-for="(wikibase, index) in wikibases" :key="index">
-				<td>{{ wikibase.wikibaseType }}</td>
+				<td>
+					{{ wikibase.wikibaseType }}
+				</td>
 				<td>{{ wikibase.title }}</td>
 				<td><LocaleNumber :stat="wikibase.quantityObservations.mostRecent?.totalTriples" /></td>
 				<td>
