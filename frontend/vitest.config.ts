defbde272f0dd1ae882c39393ae1e4bd3780b33f
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-15 13:49:54 +0200

    Coverage

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
 	viteConfig,
 	defineConfig({
 		test: {
+			coverage: {
+				provider: 'istanbul',
+				thresholds: {
+					branches: 95,
+					functions: 95,
+					lines: 95,
+					statements: 95
+				}
+			},
 			environment: 'jsdom',
 			exclude: [...configDefaults.exclude, 'e2e/**'],
 			root: fileURLToPath(new URL('./', import.meta.url))
