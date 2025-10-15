a09224b6f11d04ea9bb6ff24f6ec9b09190d5687
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-16 01:30:49 +0200

    How on earth did this affect anything

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,10 @@
-import App from '@/App.vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
+import App from './App.vue'
 
 const app = createApp(App)
 
