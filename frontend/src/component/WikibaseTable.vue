76290251c5956bfd98bbb5b78e9d3bc5eb8f8936
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-15 02:10:05 +0200

    T404553 - Table II

--- frontend/src/component/WikibaseTable.vue
+++ frontend/src/component/WikibaseTable.vue
@@ -1,53 +1,56 @@
 <script setup lang="ts">
+import LocaleNumber from '@/component/LocaleNumber.vue'
+import type {
+	WbFragment,
+	WikibaseQuantityObservationWikibaseObservationSet as WikibaseQuantityObservationSet,
+	WikibaseRecentChangesObservationWikibaseObservationSet as WikibaseRecentChangesObservationSet
+} from '@/graphql/types'
 import { useWikiStore } from '@/stores/wikibase-page-store'
+import { compareByValue } from '@/util/sortByValue'
 import { computed } from 'vue'
-import LocaleNumber from './LocaleNumber.vue'
 
 const store = useWikiStore()
 
-const wikibases = computed(() => store.wikibasePage.data?.wikibaseList.data)
+const loading = computed(() => store.wikibasePage.loading)
+const wikibases = computed<WbFragment[] | undefined>(
+	() => store.wikibasePage.data?.wikibaseList.data
+)
+
+const computeTotalEdits = (v: WikibaseRecentChangesObservationSet): number | undefined =>
+	v.mostRecent?.botChangeCount != undefined || v.mostRecent?.humanChangeCount != undefined
+		? (v.mostRecent?.botChangeCount ?? 0) + (v.mostRecent?.humanChangeCount ?? 0)
+		: undefined
+
+const headers = [
+	{ title: 'Type', value: 'wikibaseType', sortable: true },
+	{ title: 'Title', value: 'title', sortable: true },
+	{ title: 'URL', value: 'urls.baseUrl', sortable: true },
+	{
+		title: 'Triples',
+		value: 'quantityObservations',
+		sortable: true,
+		sort: (a: WikibaseQuantityObservationSet, b: WikibaseQuantityObservationSet) =>
+			compareByValue(a, b, (v) => v.mostRecent?.totalTriples)
+	},
+	{
+		title: 'Edits',
+		value: 'recentChangesObservations',
+		sortable: true,
+		sort: (a: WikibaseRecentChangesObservationSet, b: WikibaseRecentChangesObservationSet) =>
+			compareByValue(a, b, computeTotalEdits)
+	}
+]
 </script>
 
 <template>
-	<v-table v-if="wikibases" striped="even" :height="800" fixed-header fixed-footer>
-		<thead>
-			<tr>
-				<th>Type</th>
-				<th>Title</th>
-				<th>URL</th>
-				<th>Triples</th>
-				<th>Edits</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr v-for="(wikibase, index) in wikibases" :key="index">
-				<td>
-					{{ wikibase.wikibaseType }}
-				</td>
-				<td>{{ wikibase.title }}</td>
-				<td>{{ wikibase.urls.baseUrl }}</td>
-				<td><LocaleNumber :stat="wikibase.quantityObservations.mostRecent?.totalTriples" /></td>
-				<td>
-					<LocaleNumber
-						:stat="
-							wikibase.recentChangesObservations.mostRecent?.botChangeCount != undefined ||
-							wikibase.recentChangesObservations.mostRecent?.humanChangeCount != undefined
-								? (wikibase.recentChangesObservations.mostRecent?.botChangeCount ?? 0) +
-									(wikibase.recentChangesObservations.mostRecent?.humanChangeCount ?? 0)
-								: undefined
-						"
-					/>
-				</td>
-			</tr>
-		</tbody>
-	</v-table>
+	<v-data-table :items="wikibases" :headers="headers" striped="even" :loading="loading">
+		<template v-slot:item.quantityObservations="{ value }">
+			<td><LocaleNumber :stat="value.mostRecent?.totalTriples" /></td>
+		</template>
+		<template v-slot:item.recentChangesObservations="{ value }">
+			<td><LocaleNumber :stat="computeTotalEdits(value)" /></td>
+		</template>
+	</v-data-table>
 </template>
 
-<style lang="css">
-.wikibase-card-container {
-	display: flex;
-	flex-flow: row wrap;
-	gap: 6px;
-	justify-content: space-evenly;
-}
-</style>
+<style lang="css"></style>
