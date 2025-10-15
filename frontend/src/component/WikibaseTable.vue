c0d83acdaf6897a54aafe35967789c158afe2de8
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-15 02:24:29 +0200

    Default Sort

--- frontend/src/component/WikibaseTable.vue
+++ frontend/src/component/WikibaseTable.vue
@@ -7,7 +7,8 @@ import type {
 } from '@/graphql/types'
 import { useWikiStore } from '@/stores/wikibase-page-store'
 import { compareByValue } from '@/util/sortByValue'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import type { SortItem } from 'vuetify/lib/components/VDataTable/composables/sort.mjs'
 
 const store = useWikiStore()
 
@@ -16,11 +17,12 @@ const wikibases = computed<WbFragment[] | undefined>(
 	() => store.wikibasePage.data?.wikibaseList.data
 )
 
+const sortBy = ref<SortItem[]>([{ key: 'quantityObservations', order: 'asc' }])
+
 const computeTotalEdits = (v: WikibaseRecentChangesObservationSet): number | undefined =>
 	v.mostRecent?.botChangeCount != undefined || v.mostRecent?.humanChangeCount != undefined
 		? (v.mostRecent?.botChangeCount ?? 0) + (v.mostRecent?.humanChangeCount ?? 0)
 		: undefined
-
 const headers = [
 	{ title: 'Type', value: 'wikibaseType', sortable: true },
 	{ title: 'Title', value: 'title', sortable: true },
@@ -30,20 +32,26 @@ const headers = [
 		value: 'quantityObservations',
 		sortable: true,
 		sort: (a: WikibaseQuantityObservationSet, b: WikibaseQuantityObservationSet) =>
-			compareByValue(a, b, (v) => v.mostRecent?.totalTriples)
+			compareByValue(a, b, (v) => v.mostRecent?.totalTriples) * -1
 	},
 	{
 		title: 'Edits',
 		value: 'recentChangesObservations',
 		sortable: true,
 		sort: (a: WikibaseRecentChangesObservationSet, b: WikibaseRecentChangesObservationSet) =>
-			compareByValue(a, b, computeTotalEdits)
+			compareByValue(a, b, computeTotalEdits) * -1
 	}
 ]
 </script>
 
 <template>
-	<v-data-table :items="wikibases" :headers="headers" striped="even" :loading="loading">
+	<v-data-table
+		:items="wikibases"
+		:headers="headers"
+		striped="even"
+		:loading="loading"
+		v-model:sort-by="sortBy"
+	>
 		<template v-slot:item.quantityObservations="{ value }">
 			<td><LocaleNumber :stat="value.mostRecent?.totalTriples" /></td>
 		</template>
