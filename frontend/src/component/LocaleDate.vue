647f958ab2ff5ebe4b575ced993a7e0dda737d4d
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-15 17:00:10 +0200

    DateStatBlock

---
+++ frontend/src/component/LocaleDate.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import stringDate from '@/util/stringDate'
+
+defineProps<{ stat: Date | string | null | undefined }>()
+</script>
+
+<template>
+	<template v-if="stat != null && stat != undefined">
+		{{ stringDate(stat).toLocaleDateString('de') }}
+	</template>
+	<template v-else>–</template>
+</template>
+
+<style lang="css"></style>
--- frontend/src/component/wikibase-table/wikibase-detail-card/WikibaseDetailCard.vue
+++ frontend/src/component/wikibase-table/wikibase-detail-card/WikibaseDetailCard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import WikibaseCardStatBlock from '@/component/wikibase-table/wikibase-detail-card/WikibaseCardStatBlock.vue'
+import DateStatBlock from '@/component/wikibase-table/wikibase-detail-card/DateStatBlock.vue'
+import NumStatBlock from '@/component/wikibase-table/wikibase-detail-card/NumStatBlock.vue'
 import WikibaseIcon from '@/component/wikibase-table/wikibase-detail-card/WikibaseIcon.vue'
 import { useSingleWikiStore } from '@/stores/wikibase-store'
 import computeTotalEdits from '@/util/computeTotalEdits'
-import stringDate from '@/util/stringDate'
 import { computed, onBeforeMount } from 'vue'
 
 const props = defineProps<{ wikibaseId: number }>()
@@ -16,104 +16,86 @@ onBeforeMount(() => store.searchWikibase(props.wikibaseId))
 </script>
 
 <template>
-	<v-skeleton-loader
-		class="wikibase-detail-card-loader"
-		v-if="loading"
-		color="secondary"
-		type="card"
-	>
-		LOADING
-	</v-skeleton-loader>
-	<v-card v-if="wikibase" variant="outlined" class="wikibase-detail-card ma-1 pa-1">
-		<v-container class="card-header ma-0 pa-0">
-			<v-container class="ma-0 pa-0">
-				<v-container class="url-container ma-0 pa-0">
-					<WikibaseIcon :base-url="wikibase.urls.baseUrl" />
-					<v-container class="wikibase-url ma-0 pa-0">
-						<a :href="wikibase.urls.baseUrl">
-							{{ wikibase.title }}
-						</a>
+	<v-card variant="outlined" class="wikibase-detail-card ma-1 pa-1">
+		<v-skeleton-loader
+			class="wikibase-detail-card-loader"
+			v-if="loading"
+			color="secondary"
+			type="card"
+		>
+			LOADING
+		</v-skeleton-loader>
+		<template v-else-if="wikibase">
+			<v-container class="card-header ma-0 pa-0">
+				<v-container class="ma-0 pa-0">
+					<v-container class="url-container ma-0 pa-0">
+						<WikibaseIcon :base-url="wikibase.urls.baseUrl" />
+						<v-container class="wikibase-url ma-0 pa-0">
+							<a :href="wikibase.urls.baseUrl">
+								{{ wikibase.title }}
+							</a>
+						</v-container>
+						<v-container v-if="wikibase.urls.sparqlFrontendUrl" class="wikibase-url ma-0 pa-0">
+							<a :href="wikibase.urls.sparqlFrontendUrl"> SPARQL </a>
+						</v-container>
 					</v-container>
-					<v-container v-if="wikibase.urls.sparqlFrontendUrl" class="wikibase-url ma-0 pa-0">
-						<a :href="wikibase.urls.sparqlFrontendUrl"> SPARQL </a>
-					</v-container>
+					<div v-if="wikibase.description" class="description">{{ wikibase.description }}</div>
 				</v-container>
-				<div v-if="wikibase.description" class="description">{{ wikibase.description }}</div>
+				<v-container class="wikibase-type ma-0 pa-0">{{ wikibase.wikibaseType }}</v-container>
 			</v-container>
-			<v-container class="wikibase-type ma-0 pa-0">{{ wikibase.wikibaseType }}</v-container>
-		</v-container>
-		<v-container
-			v-if="wikibase.timeToFirstValueObservations.mostRecent?.initiationDate"
-			class="stat-block-container ma-0 pa-0"
-		>
-			<v-label>
-				FIRST RECORD
-				{{
-					stringDate(
-						wikibase.timeToFirstValueObservations.mostRecent?.initiationDate
-					).toLocaleDateString()
-				}}
-			</v-label>
-			<v-label>
-				AS OF
-				{{
-					stringDate(
-						wikibase.timeToFirstValueObservations.mostRecent?.observationDate
-					).toLocaleDateString()
-				}}
-			</v-label>
-		</v-container>
-		<v-container
-			v-if="wikibase.quantityObservations.mostRecent"
-			class="stat-block-container ma-0 pa-0"
-		>
-			<WikibaseCardStatBlock
-				label="ITEMS"
-				:stat="wikibase.quantityObservations.mostRecent?.totalItems"
-			/>
-			<WikibaseCardStatBlock
-				label="PROPERTIES"
-				:stat="wikibase.quantityObservations.mostRecent?.totalProperties"
-			/>
-			<WikibaseCardStatBlock
-				label="LEXEMES"
-				:stat="wikibase.quantityObservations.mostRecent?.totalLexemes"
-			/>
-			<WikibaseCardStatBlock
-				label="TRIPLES"
-				:stat="wikibase.quantityObservations.mostRecent?.totalTriples"
-			/>
-			<v-label>
-				AS OF
-				{{
-					stringDate(wikibase.quantityObservations.mostRecent?.observationDate).toLocaleDateString()
-				}}
-			</v-label>
-		</v-container>
-		<v-container
-			v-if="wikibase.recentChangesObservations.mostRecent"
-			class="stat-block-container ma-0 pa-0"
-		>
-			<WikibaseCardStatBlock
-				label="EDITS (30 DAYS)"
-				:stat="computeTotalEdits(wikibase.recentChangesObservations)"
-			/>
-			<v-label>
-				AS OF
-				{{
-					stringDate(
-						wikibase.recentChangesObservations.mostRecent?.observationDate
-					).toLocaleDateString()
-				}}
-			</v-label>
-		</v-container>
+			<v-container
+				v-if="wikibase.timeToFirstValueObservations.mostRecent?.initiationDate"
+				class="stat-block-container pa-0"
+			>
+				<DateStatBlock
+					label="First Record"
+					:stat="wikibase.timeToFirstValueObservations.mostRecent?.initiationDate"
+				/>
+				<DateStatBlock
+					label="AS OF"
+					:stat="wikibase.timeToFirstValueObservations.mostRecent?.observationDate"
+				/>
+			</v-container>
+			<v-container
+				v-if="wikibase.quantityObservations.mostRecent"
+				class="stat-block-container pa-0"
+			>
+				<NumStatBlock label="ITEMS" :stat="wikibase.quantityObservations.mostRecent?.totalItems" />
+				<NumStatBlock
+					label="PROPERTIES"
+					:stat="wikibase.quantityObservations.mostRecent?.totalProperties"
+				/>
+				<NumStatBlock
+					label="LEXEMES"
+					:stat="wikibase.quantityObservations.mostRecent?.totalLexemes"
+				/>
+				<NumStatBlock
+					label="TRIPLES"
+					:stat="wikibase.quantityObservations.mostRecent?.totalTriples"
+				/>
+				<DateStatBlock
+					label="AS OF"
+					:stat="wikibase.quantityObservations.mostRecent?.observationDate"
+				/>
+			</v-container>
+			<v-container
+				v-if="wikibase.recentChangesObservations.mostRecent"
+				class="stat-block-container pa-0"
+			>
+				<NumStatBlock
+					label="EDITS (30 DAYS)"
+					:stat="computeTotalEdits(wikibase.recentChangesObservations)"
+				/>
+				<DateStatBlock
+					label="AS OF"
+					:stat="wikibase.recentChangesObservations.mostRecent?.observationDate"
+				/>
+			</v-container>
+		</template>
 	</v-card>
 </template>
 
 <style lang="css">
-.wikibase-detail-card-loader {
-	background-color: white;
-}
 .wikibase-detail-card {
 	display: flex;
 	flex-flow: column nowrap;
@@ -145,6 +127,8 @@ onBeforeMount(() => store.searchWikibase(props.wikibaseId))
 .stat-block-container {
 	display: flex;
 	flex-flow: row wrap;
+	border: 2px solid grey;
+	margin: 6px 0;
 }
 .stat-block-container .stat-container,
 .stat-block-container .v-label {
