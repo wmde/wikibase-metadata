c055cf6f776ce5877be4a670af1623a079539aea
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-16 02:46:55 +0200

    Test wikibase-page-store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AppHeader from '@/component/AppHeader.vue';
-import WikibaseTableContainer from '@/component/WikibaseTableContainer.vue';
+import AppHeader from '@/component/AppHeader.vue'
+import WikibaseTableContainer from '@/component/WikibaseTableContainer.vue'
 </script>
 
 <template>
---
+++ frontend/src/stores/__tests__/wikibase-page-store.spec.ts
@@ -0,0 +1,58 @@
+import { WikibaseType } from '@/graphql/types'
+import { createPinia, setActivePinia } from 'pinia'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { nextTick } from 'vue'
+import { useWikiStore } from '../wikibase-page-store'
+
+const { mockLoad, mockUseLazyQuery } = vi.hoisted(() => ({
+	mockLoad: vi.fn().mockName('load'),
+	mockUseLazyQuery: vi.fn().mockName('useLazyQuery')
+}))
+
+vi.mock('@vue/apollo-composable', () => ({
+	provideApolloClient: vi.fn().mockName('provideApolloClient'),
+	useLazyQuery: mockUseLazyQuery.mockReturnValueOnce({
+		load: mockLoad,
+		result: { value: { wikibaseList: { meta: { totalCount: 0 }, data: [] } } },
+		loading: { value: false },
+		error: { value: false }
+	})
+}))
+
+describe('useWikiStore', async () => {
+	beforeEach(() => {
+		vi.resetAllMocks()
+		setActivePinia(createPinia())
+	})
+
+	it('reflects query results', async () => {
+		const store = useWikiStore()
+
+		expect(store.pageNumber).toEqual(1)
+		expect(store.pageSize).toEqual(10000)
+		expect(store.wikibaseFilter).toEqual({ wikibaseType: { exclude: [WikibaseType.Test] } })
+		expect(store.wikibasePage).toEqual({
+			data: { wikibaseList: { data: [], meta: { totalCount: 0 } } },
+			errorState: false,
+			loading: false
+		})
+	})
+
+	it('calls load on fetchWikibasePage', async () => {
+		const store = useWikiStore()
+
+		expect(mockLoad).toHaveBeenCalledTimes(0)
+		store.fetchWikibasePage()
+		expect(mockLoad).toHaveBeenCalledTimes(1)
+	})
+
+	it('calls load on excludeWikibaseTypes', async () => {
+		const store = useWikiStore()
+
+		expect(mockLoad).toHaveBeenCalledTimes(0)
+
+		store.excludeWikibaseTypes([WikibaseType.Cloud])
+		await nextTick()
+		expect(mockLoad).toHaveBeenCalledTimes(1)
+	})
+})
