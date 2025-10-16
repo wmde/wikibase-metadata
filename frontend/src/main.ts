32ea4eca2d85de8a31cc16d4f4089c9394e0f618
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-16 19:10:14 +0200

    Icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,17 @@ import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import 'vuetify/styles'
 import App from './App.vue'
 
 const app = createApp(App)
 
-export const vuetify = createVuetify({ components, directives })
+export const vuetify = createVuetify({
+	components,
+	directives,
+	icons: { defaultSet: 'mdi', aliases, sets: { mdi } }
+})
 app.use(vuetify)
 
 app.use(createPinia())
