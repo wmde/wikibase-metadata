ff6ab7c4aeaebb34a49c1e4454fd9f3dc2c992f4
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-21 15:43:15 +0200

    Populate Backend URL

--- frontend/src/stores/client.ts
+++ frontend/src/stores/client.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client/core'
 
 const cache = new InMemoryCache()
-const cloudLink = ApolloLink.from([
-	new HttpLink({ uri: 'https://wikibase-metadata.wmcloud.org/graphql' })
-])
+const cloudLink = ApolloLink.from([new HttpLink({ uri: import.meta.env.VITE_BACKEND_URL })])
 
 export const apolloClient = new ApolloClient({ cache, link: cloudLink })
