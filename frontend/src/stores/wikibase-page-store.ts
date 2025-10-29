5e72575574760958623745a87d0d31f37ee0edc6
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-10-29 23:08:49 +0100

    Store

--- frontend/src/stores/wikibase-page-store.ts
+++ frontend/src/stores/wikibase-page-store.ts
@@ -1,9 +1,12 @@
 import { pageWikibasesQuery } from '@/graphql/queries/wikibase-list-query'
 import {
+	SortColumn,
+	SortDirection,
 	WikibaseType,
 	type PageWikibasesQuery,
 	type PageWikibasesQueryVariables,
-	type WikibaseFilterInput
+	type WikibaseFilterInput,
+	type WikibaseSortInput
 } from '@/graphql/types'
 import { apolloClient } from '@/stores/client'
 import type { QueryResult } from '@/stores/query-result'
@@ -18,11 +21,13 @@ export type WikibasePageStoreType = {
 	wikibasePage:
 		| QueryResult<PageWikibasesQuery | undefined>
 		| Ref<QueryResult<PageWikibasesQuery | undefined>>
-	pageNumber: number
+	pageNumber: number | Ref<number>
 	setPageNumber: (i: number) => void
-	pageSize: number
+	pageSize: number | Ref<number>
 	setPageSize: (i: number) => void
-	wikibaseFilter: WikibaseFilterInput
+	sortBy: WikibaseSortInput | undefined | Ref<WikibaseSortInput | undefined>
+	setSort: (sortBy: WikibaseSortInput | undefined) => void
+	wikibaseFilter: WikibaseFilterInput | Ref<WikibaseFilterInput>
 	includeWikibaseTypes: (t: WikibaseType[]) => void
 }
 
@@ -44,6 +49,11 @@ export const useWikiStore = defineStore('wiki-list', (): WikibasePageStoreType =
 	const pageSize = ref(10)
 	const setPageSize = (i: number) => (pageSize.value = i)
 
+	const sortBy = ref<WikibaseSortInput | undefined>({
+		column: SortColumn.Triples,
+		dir: SortDirection.Desc
+	})
+	const setSort = (val: WikibaseSortInput | undefined) => (sortBy.value = val)
 
 	const wikibaseFilter = ref<WikibaseFilterInput>({
 		wikibaseType: { include: [WikibaseType.Cloud, WikibaseType.Suite, WikibaseType.Unknown] }
@@ -55,20 +65,24 @@ export const useWikiStore = defineStore('wiki-list', (): WikibasePageStoreType =
 		load(pageWikibasesQuery, {
 			pageNumber: pageNumber.value,
 			pageSize: pageSize.value,
+			sortBy: sortBy.value,
 			wikibaseFilter: wikibaseFilter.value
 		})
 	watch(pageNumber, () => fetchWikibasePage())
 	watch(pageSize, () => fetchWikibasePage())
+	watch(sortBy, () => fetchWikibasePage())
 	watch(wikibaseFilter, () => fetchWikibasePage())
 
 	return {
 		fetchWikibasePage,
 		wikibasePage,
-		pageNumber: pageNumber.value,
+		pageNumber,
 		setPageNumber,
-		pageSize: pageSize.value,
+		pageSize,
 		setPageSize,
-		wikibaseFilter: wikibaseFilter.value,
+		sortBy,
+		setSort,
+		wikibaseFilter,
 		includeWikibaseTypes
 	}
 })
