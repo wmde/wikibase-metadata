dee83940fa17e5982b7169f003f4a8777ae5689a
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-04 22:37:38 +0100

    Category Sort

---
+++ tests/test_query/test_wikibase_list_sort_category_query.py
@@ -0,0 +1,89 @@
+"""Test Sort Wikibase List"""
+
+import pytest
+from tests.test_query.wikibase_list_query import WIKIBASE_LIST_QUERY
+from tests.test_schema import test_schema
+from tests.utils import assert_layered_property_value, assert_page_meta
+
+
+# @pytest.mark.skip
+@pytest.mark.asyncio
+@pytest.mark.query
+@pytest.mark.dependency(
+    name="sort-cat-asc",
+    depends=["mutate-cloud-instances"],
+    scope="session",
+)
+async def test_wikibase_list_query_sort_category_asc():
+    """Test Sort Category Ascending"""
+
+    result = await test_schema.execute(
+        WIKIBASE_LIST_QUERY,
+        variable_values={
+            "pageNumber": 1,
+            "pageSize": 11,
+            "sortBy": {"column": "CATEGORY", "dir": "ASC"},
+        },
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+    assert "wikibaseList" in result.data
+    assert_page_meta(result.data["wikibaseList"], 1, 11, 11, 1)
+    for i in range(9):
+        assert_layered_property_value(
+            result.data, ["wikibaseList", "data", i, "category"], None
+        )
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 9, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 10, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+@pytest.mark.query
+@pytest.mark.dependency(
+    name="sort-cat-desc",
+    depends=[
+        "mutate-cloud-instances",
+        "sort-cat-asc",
+    ],
+    scope="session",
+)
+async def test_wikibase_list_query_sort_category_desc():
+    """Test Sort Category Descending"""
+
+    result = await test_schema.execute(
+        WIKIBASE_LIST_QUERY,
+        variable_values={
+            "pageNumber": 1,
+            "pageSize": 11,
+            "sortBy": {"column": "CATEGORY", "dir": "DESC"},
+        },
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+    assert "wikibaseList" in result.data
+    assert_page_meta(result.data["wikibaseList"], 1, 11, 11, 1)
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 0, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 1, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+    for i in range(2, 11):
+        assert_layered_property_value(
+            result.data, ["wikibaseList", "data", i, "category"], None
+        )
