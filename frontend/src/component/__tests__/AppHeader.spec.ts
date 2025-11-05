0f194fcf1b6ce458714d438a0354e8a62c23e15a
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-05 12:57:14 +0100

    Revert "Tests"
    This reverts commit 9e95a18246188892b0beb1cc0c287b3d06f89add.

--- frontend/src/component/__tests__/AppHeader.spec.ts
+++ frontend/src/component/__tests__/AppHeader.spec.ts
@@ -10,28 +10,14 @@ describe('AppHeader', async () => {
 		const headerContainer = wrapper.find('div.header')
 		expect(headerContainer.exists()).toEqual(true)
 
-		const leftSection = headerContainer.find('div.left-header')
-		expect(leftSection.exists()).toEqual(true)
-		expect(leftSection.classes()).toContain('shrink')
-
-		const iconContainer = leftSection.find('div.icon')
+		const iconContainer = headerContainer.find('div.icon')
 		expect(iconContainer.exists()).toEqual(true)
-		expect(iconContainer.classes()).toContain('shrink')
 
-		const titleContainer = leftSection.find('div.title')
+		const titleContainer = headerContainer.find('div.title')
 		expect(titleContainer.classes()).toContain('v-container')
 		expect(titleContainer.classes()).toContain('ma-0')
 		expect(titleContainer.classes()).toContain('pa-0')
-		expect(titleContainer.classes()).toContain('shrink')
 
 		expect(titleContainer.text()).toEqual('Suite Scraper')
-
-		const rightSection = headerContainer.find('div.right-header')
-		expect(rightSection.exists()).toEqual(true)
-		expect(rightSection.classes()).toContain('shrink')
-
-		const themeButton = rightSection.find('button.theme-switch')
-		expect(themeButton.exists()).toEqual(true)
-		expect(themeButton.find('i.v-icon').exists()).toEqual(true)
 	})
 })
