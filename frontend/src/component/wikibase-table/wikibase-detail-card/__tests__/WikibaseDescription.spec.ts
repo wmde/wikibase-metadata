84fb9c9245f4bd7441ac9f027ef9a35a0fe5705e
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-07 13:25:10 +0100

    Test Description

---
+++ frontend/src/component/wikibase-table/wikibase-detail-card/__tests__/WikibaseDescription.spec.ts
@@ -0,0 +1,39 @@
+import { WikibaseCategory, WikibaseType } from '@/graphql/types'
+import vuetify from '@/plugin/vuetify'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import WikibaseDescription from '../WikibaseDescription.vue'
+
+describe('WikibaseDescription', async () => {
+	it('renders description properly', async () => {
+		const wrapper = mount(WikibaseDescription, {
+			global: { plugins: [vuetify] },
+			props: {
+				wikibase: {
+					id: '1',
+					title: 'Test Wikibase',
+					category: WikibaseCategory.FictionalAndCreativeWorks,
+					description: 'A test description',
+					urls: {
+						baseUrl: 'https://test-wikibase-001.test'
+					},
+					quantityObservations: {},
+					recentChangesObservations: {},
+					timeToFirstValueObservations: {},
+					wikibaseType: WikibaseType.Cloud
+				}
+			}
+		})
+
+		const tooltip = document.querySelector('.desc-tooltip')
+		expect(tooltip).not.toBeNull()
+		expect(tooltip?.textContent).toEqual('Manually written')
+
+		const descContainer = wrapper.find('div.description')
+		expect(descContainer.exists()).toEqual(true)
+		expect(descContainer.classes()).toContain('v-container')
+		expect(descContainer.classes()).toContain('ma-0')
+		expect(descContainer.classes()).toContain('pa-1')
+		expect(descContainer.text()).toEqual('A test description')
+	})
+})
