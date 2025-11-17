ebfd9ec39614f217c0420623fed8cd0fb9cc4fac
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-17 12:32:48 +0100

    Progress

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
 alembic upgrade head && \
-# PYTHONPATH=. pytest -m data -p no:cacheprovider && \
+PYTHONPATH=. pytest -m data -p no:cacheprovider && \
 gunicorn app:app -k uvicorn.workers.UvicornWorker --workers 9 -b 0.0.0.0:8000 --preload
