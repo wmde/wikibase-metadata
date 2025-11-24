194af74dd0ce2fdc7235774e3d29bf99c5c2a539
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-24 07:10:44 +0100

    Args

--- backend/data/database_connection.py
+++ backend/data/database_connection.py
@@ -10,9 +10,9 @@ from config import database_connection_string
 
 async_engine = create_async_engine(
     database_connection_string,
-    pool_size=5,  # default 5
-    max_overflow=10,  # default 10
-    pool_timeout=120,  # default 30, but we need more time for big queries, toolforge is slow
+    # pool_size=5,  # default 5
+    # max_overflow=10,  # default 10
+    # pool_timeout=120,  # default 30, but we need more time for big queries, toolforge is slow
     connect_args={"timeout": 30},  # Wait seconds for a lock before failing
     poolclass=NullPool,
 )
