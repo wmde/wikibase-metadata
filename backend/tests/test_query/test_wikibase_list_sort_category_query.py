b3cb913c2ca481e8c006fc875bef70d00b80999d
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-11-25 12:28:05 +0100

    Test Desc

--- backend/tests/test_query/test_wikibase_list_sort_category_query.py
+++ backend/tests/test_query/test_wikibase_list_sort_category_query.py
@@ -42,3 +42,41 @@ async def test_wikibase_list_query_sort_category_asc():
         ["wikibaseList", "data", 10, "category"],
         "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
     )
+
+
+@pytest.mark.asyncio
+@pytest.mark.query
+@pytest.mark.dependency(
+    name="sort-cat-asc", depends=["mutate-cloud-instances"], scope="session"
+)
+async def test_wikibase_list_query_sort_category_desc():
+    """Test Sort Category Descending"""
+
+    result = await test_schema.execute(
+        WIKIBASE_LIST_QUERY,
+        variable_values={
+            "pageNumber": 1,
+            "pageSize": 11,
+            "sortBy": {"column": "CATEGORY", "dir": "DESC"},
+        },
+    )
+
+    assert result.errors is None
+    assert result.data is not None
+    assert "wikibaseList" in result.data
+    assert_page_meta(result.data["wikibaseList"], 1, 11, 11, 1)
+    assert result.data["wikibaseList"]["data"] == {}
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 0, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+    assert_layered_property_value(
+        result.data,
+        ["wikibaseList", "data", 1, "category"],
+        "EXPERIMENTAL_AND_PROTOTYPE_PROJECTS",
+    )
+    for i in range(2, 11):
+        assert_layered_property_value(
+            result.data, ["wikibaseList", "data", i, "category"], None
+        )
