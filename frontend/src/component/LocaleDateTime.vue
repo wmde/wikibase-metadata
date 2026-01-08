0fbc96fbcf6e76d248ac8a2e4bd086e8d875285a
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-01-08 12:01:39 +0100

    Display Data

---
+++ frontend/src/component/LocaleDateTime.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import stringDate from '@/util/string-date'
+
+defineProps<{ stat: Date | string | null | undefined }>()
+</script>
+
+<template>
+	<template v-if="stat != null && stat != undefined">
+		{{ stringDate(stat).toLocaleDateString('de') }}
+		{{ stringDate(stat).toLocaleTimeString('de') }}
+	</template>
+	<template v-else>–</template>
+</template>
