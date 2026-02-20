a5c7e845eb2a9d25278a80249e0cb81bfcc6d63a
Jacob Thwaites <jacob.thwaites@wikimedia.de>
2026-02-20 16:09:08 +0100

    move scheduler into its own docker service

---
+++ backend/schedule/run_scheduler.py
@@ -0,0 +1,23 @@
+import asyncio
+from functools import partial
+
+from schedule.scheduler import create_scheduler, health_handler
+
+
+async def main():
+    """Create and run the scheduler server"""
+    scheduler = create_scheduler()
+    scheduler.start()
+
+    server = await asyncio.start_server(
+        partial(health_handler, scheduler=scheduler),
+        "0.0.0.0",
+        8001,
+    )
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
