dcb4f007bd9235a541bffcee5000574625b489e5
Jacob Thwaites <jacob.thwaites@wikimedia.de>
2026-03-13 15:50:44 +0100

    use nullpool database connection for testing

--- backend/data/database_connection.py
+++ backend/data/database_connection.py
@@ -5,11 +5,13 @@ from contextlib import asynccontextmanager
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from config import database_connection_string
+from sqlalchemy.pool import NullPool
+from config import database_connection_string, is_testing
 
 async_engine = create_async_engine(
     database_connection_string,
     connect_args={"timeout": 30},
+    poolclass=NullPool if is_testing else None,
 )
 
 async_session = sessionmaker(
@@ -31,5 +33,3 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
         except Exception:
             await session.rollback()
             raise
-        finally:
-            await session.close()
