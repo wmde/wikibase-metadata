7e058d6f7ef7011527ef0f0dcd8b964f28368e5b
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-09 01:00:02 +0200

    Reorder

--- backend/tests/test_mutation/test_update_wikibase_reuse.py
+++ backend/tests/test_mutation/test_update_wikibase_reuse.py
@@ -40,6 +40,58 @@ mutation MyMutation($wikibaseId: Int!, $reuse: Boolean!) {
 @pytest.mark.asyncio
 @pytest.mark.mutation
 @pytest.mark.dependency(
+    name="wikibase-set-reuse-false",
+    depends=["transform-cloud-instance"],
+)
+async def test_set_wikibase_reuse_false():
+    """Set Wikibase Reuse False"""
+
+    before_adding_result = await test_schema.execute(
+        WIKIBASE_FILTERED_AND_UNFILTERED_QUERY
+    )
+    assert before_adding_result.errors is None
+    assert before_adding_result.data is not None
+    assert_layered_property_value(
+        before_adding_result.data, ["filtered", "meta", "totalCount"], expected_value=2
+    )
+    assert_layered_property_value(
+        before_adding_result.data,
+        ["unfiltered", "meta", "totalCount"],
+        expected_value=3,
+    )
+
+    before_adding_reuse_true_ids = {
+        int(w["id"]) for w in before_adding_result.data["filtered"]["data"]
+    }
+
+    update_result = await test_schema.execute(
+        UPDATE_WIKIBASE_REUSE_MUTATION,
+        variable_values={
+            "wikibaseId": before_adding_reuse_true_ids.pop(),
+            "reuse": True,
+        },
+        context_value=get_mock_context("test-auth-token"),
+    )
+    assert update_result.errors is None
+    assert update_result.data is not None
+    assert update_result.data["setReuseFlag"] is True
+
+    after_adding_result = await test_schema.execute(
+        WIKIBASE_FILTERED_AND_UNFILTERED_QUERY
+    )
+    assert after_adding_result.errors is None
+    assert after_adding_result.data is not None
+    assert_layered_property_value(
+        after_adding_result.data, ["filtered", "meta", "totalCount"], expected_value=1
+    )
+    assert_layered_property_value(
+        after_adding_result.data, ["unfiltered", "meta", "totalCount"], expected_value=3
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.mutation
+@pytest.mark.dependency(
     name="wikibase-set-reuse-true",
     depends=["transform-cloud-instance", "wikibase-set-reuse-false"],
     scope="session",
@@ -152,56 +204,4 @@ async def test_set_cloud_wikibase_reuse_true():
         after_adding_result.data,
         ["unfiltered", "meta", "totalCount"],
         expected_value=11,
-    )
-
-
-@pytest.mark.asyncio
-@pytest.mark.mutation
-@pytest.mark.dependency(
-    name="wikibase-set-reuse-false",
-    depends=["transform-cloud-instance"],
-)
-async def test_set_wikibase_reuse_false():
-    """Set Wikibase Reuse False"""
-
-    before_adding_result = await test_schema.execute(
-        WIKIBASE_FILTERED_AND_UNFILTERED_QUERY
-    )
-    assert before_adding_result.errors is None
-    assert before_adding_result.data is not None
-    assert_layered_property_value(
-        before_adding_result.data, ["filtered", "meta", "totalCount"], expected_value=2
-    )
-    assert_layered_property_value(
-        before_adding_result.data,
-        ["unfiltered", "meta", "totalCount"],
-        expected_value=3,
-    )
-
-    before_adding_reuse_true_ids = {
-        int(w["id"]) for w in before_adding_result.data["filtered"]["data"]
-    }
-
-    update_result = await test_schema.execute(
-        UPDATE_WIKIBASE_REUSE_MUTATION,
-        variable_values={
-            "wikibaseId": before_adding_reuse_true_ids.pop(),
-            "reuse": True,
-        },
-        context_value=get_mock_context("test-auth-token"),
-    )
-    assert update_result.errors is None
-    assert update_result.data is not None
-    assert update_result.data["setReuseFlag"] is True
-
-    after_adding_result = await test_schema.execute(
-        WIKIBASE_FILTERED_AND_UNFILTERED_QUERY
-    )
-    assert after_adding_result.errors is None
-    assert after_adding_result.data is not None
-    assert_layered_property_value(
-        after_adding_result.data, ["filtered", "meta", "totalCount"], expected_value=1
-    )
-    assert_layered_property_value(
-        after_adding_result.data, ["unfiltered", "meta", "totalCount"], expected_value=3
     )
