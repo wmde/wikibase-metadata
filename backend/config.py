f138fa544f3ef061c6e054490eb32b65537c39f6
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-09 02:01:35 +0200

    quasi-local

--- backend/config.py
+++ backend/config.py
@@ -12,12 +12,12 @@ assert auth_token is not None, "No Authentication Token Found"
 database_connection_string = os.path.expandvars(
     config.get("database", "database_connection_string")
 )
-config_old_database_connection_string_var = config.get(
+_old_database_connection_string_var = config.get(
     "database", "old_database_connection_string", fallback=None
 )
 old_database_connection_string = (
-    os.path.expandvars(config_old_database_connection_string_var)
-    if config_old_database_connection_string_var is not None
+    os.path.expandvars(_old_database_connection_string_var)
+    if _old_database_connection_string_var is not None
     else None
 )
 
