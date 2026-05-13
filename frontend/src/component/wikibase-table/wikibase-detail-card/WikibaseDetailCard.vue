48108b3f171ea9279ba9931f7ff008be4f67fad2
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-13 12:18:24 +0200

    Unused CSS

--- frontend/src/component/wikibase-table/wikibase-detail-card/WikibaseDetailCard.vue
+++ frontend/src/component/wikibase-table/wikibase-detail-card/WikibaseDetailCard.vue
@@ -38,7 +38,7 @@ const theme = useTheme()
 
 <style lang="css">
 .wikibase-detail-card {
-	max-width: 500px;
+	max-width: 600px;
 	border-color: rgba(255, 255, 255, 0.12);
 	border-radius: 0.625rem !important;
 }
@@ -46,52 +46,5 @@ const theme = useTheme()
 	display: flex;
 	flex-flow: row wrap;
 	gap: 0.75rem;
-}
-.card-header {
-	display: flex;
-	flex-flow: row nowrap;
-	gap: 12px;
-}
-.url-container {
-	display: flex;
-	flex-flow: row wrap;
-	justify-content: flex-start;
-	gap: 4px;
-	align-items: center;
-}
-.url-container a {
-	color: inherit;
-}
-.wikibase-url {
-	width: auto;
-}
-.wikibase-type {
-	width: auto;
-	align-self: stretch;
-	align-content: center;
-}
-.wikibase-type-chip {
-	min-width: 40px;
-	font-weight: 500;
-}
-.stat-block-container {
-	border: 2px solid grey;
-	margin: 6px 0;
-}
-.stats-container {
-	display: flex;
-	flex-flow: column nowrap;
-	margin: 0;
-	padding: 0;
-}
-.stat-source-container {
-	color: grey;
-	/* margin: 0; */
-	/* padding: 4px; */
-}
-.stat-block-container .stat-container,
-.stat-block-container .v-label {
-	width: auto;
-	margin: 4px;
 }
 </style>
