7329d46b17b87586e06352b34069b22dbeef4a31
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-05-18 12:09:16 +0200

    Display Data

--- frontend/src/component/WikibaseTableContainer.vue
+++ frontend/src/component/WikibaseTableContainer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import WikibaseTable from '@/component/wikibase-table/WikibaseTable.vue'
+import WikibaseTotalContainer from '@/component/wikibase-table/WikibaseTotalContainer.vue'
 import { useWikiStore } from '@/stores/wikibase-page-store'
 import { computed, onBeforeMount } from 'vue'
 
@@ -15,6 +16,7 @@ onBeforeMount(() => store.fetchWikibasePage())
 <template>
 	<v-container class="wikibase-table-container my-0 px-6 py-8">
 		<v-alert v-if="error" type="error" variant="tonal" title="Error">Error fetching data</v-alert>
+		<wikibase-total-container />
 		<v-container v-if="totalCount && showCount" class="show-count mb-6 pa-0">
 			Showing {{ showCount.toLocaleString('en') }} of
 			{{ totalCount.toLocaleString('en') }} instances
---
+++ frontend/src/component/wikibase-table/WikibaseTotalBox.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import powerOfThousand from '@/util/power-of-thousand'
+
+const { value } = defineProps<{ label: string; value: number }>()
+const split = powerOfThousand(value)
+const powerString = (power: number) => {
+	const dict: Record<number, string> = { 0: '', 1: 'Th', 2: 'M', 3: 'B', 4: 'T' }
+	return dict[power]
+}
+const roundValue = (value: number) => Math.round(value * 10) / 10
+</script>
+
+<template>
+	<v-container class="wikibase-total-box ma-0 pa-6">
+		<v-container class="value ma-0 pa-0 mb-1">
+			<span>
+				{{ roundValue(split.value) }}
+			</span>
+			<span>
+				{{ powerString(split.power) }}
+			</span>
+		</v-container>
+		<v-container class="label ma-0 pa-0">{{ label }}</v-container>
+	</v-container>
+</template>
+
+<style lang="scss">
+.wikibase-total-box {
+	width: auto;
+	flex-grow: 1;
+	background-color: #fff;
+	border: 1px solid rgba(0, 0, 0, 0.1);
+	border-radius: 0.625rem;
+	.label {
+		font-family: Roboto;
+		font-size: 16px;
+		color: rgb(0, 0, 0);
+	}
+	.value {
+		font-family: Montserrat;
+		color: rgb(0, 0, 0);
+		font-weight: 700;
+		font-size: 1.875rem;
+		line-height: calc(2.25 / 1.875);
+	}
+}
+</style>
