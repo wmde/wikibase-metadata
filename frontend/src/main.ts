79a56b79421f3c5b5ce215f9fb4064ec6ec79271
Jacob Thwaites <jacob.thwaites@wikimedia.de>
2026-06-01 15:56:32 +0200

    add matomo tracking to frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,17 @@
 import vuetify from '@/plugin/vuetify'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import VueMatomo from 'vue-matomo'
 import App from './App.vue'
 
 const app = createApp(App)
+
+createApp(App)
+	.use(VueMatomo, {
+		host: 'stats.wikimedia.de',
+		siteId: 12
+	})
+	.mount('#app')
 
 app.use(vuetify)
 
