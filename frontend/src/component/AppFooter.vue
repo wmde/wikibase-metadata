5708d25ea756cb9b24cf4ca007765f1273294d47
Jacob Thwaites <jacob.thwaites@wikimedia.de>
2026-06-10 11:58:35 +0200

    add footer with imprint and data protection links

---
+++ frontend/src/component/AppFooter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import img from '@/media/icon/wikibase_symbol_RGB.png'
+import { mdiThemeLightDark } from '@mdi/js'
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+</script>
+
+<template>
+	<v-container id="app-footer" class="py-6">
+        <a class="footer-link" href="https://www.wikimedia.de/impressum/">Imprint</a>
+        <a class="footer-link" href="https://www.wikimedia.de/datenschutz/#dataprivacy">Data Protection</a>
+	</v-container>
+</template>
+
+<style lang="scss">
+    #app-footer {
+        display: flex;
+        justify-content: center;
+        gap: 32px;
+    }
+
+    .footer-link {
+        text-decoration: none;
+    }
+</style>
