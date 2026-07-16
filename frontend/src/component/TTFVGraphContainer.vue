ac2cf84fdd90142d524f3314fc60f681428e4c86
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-07-16 14:59:47 +0200

    ExistingQGraph

--- frontend/src/component/TTFVGraphContainer.vue
+++ frontend/src/component/TTFVGraphContainer.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
+import QGraph from '@/component/graph/QGraph.vue'
 import { useWikiTTFVStore } from '@/stores/wikibase-ttfv-store'
+import stringDate from '@/util/string-date'
 import { computed, onBeforeMount } from 'vue'
 
 const store = useWikiTTFVStore()
-const data = computed(() => store.wikibases.data)
+const wikibase = computed(() => store.wikibases.data?.[0])
 
 onBeforeMount(() => store.fetchWikibases())
 </script>
 
 <template>
 	<v-container class="ttfv-graph-container my-0 px-6 py-8">
-		<v-container>{{ data }}</v-container>
+		<v-container>{{ wikibase }}</v-container>
+			<q-graph
+				v-if="wikibase?.timeToFirstValueObservations.mostRecent?.initiationDate"
+				:dataset="{
+					label: 'Q',
+					data: [
+						{
+							x: stringDate(
+								wikibase.timeToFirstValueObservations.mostRecent.initiationDate
+							).valueOf(),
+							y: 0.5
+						},
+						...wikibase.timeToFirstValueObservations.mostRecent.itemDates.map((v) => ({
+							x: stringDate(v.creationDate).valueOf(),
+							y: v.q
+						}))
+					]
+				}"
+			/>
 	</v-container>
 </template>
 
