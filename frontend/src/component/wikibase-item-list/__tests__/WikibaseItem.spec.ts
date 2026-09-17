93c8f7b13d05a43f4d757d543f5d2de22be48eb9
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-17 17:51:23 +0200

    Completion

--- frontend/src/component/wikibase-item-list/__tests__/WikibaseItem.spec.ts
+++ frontend/src/component/wikibase-item-list/__tests__/WikibaseItem.spec.ts
@@ -4,7 +4,7 @@ import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 
 describe('WikibaseItem', async () => {
-	it(`renders $arg properly`, async () => {
+	it(`renders properly with scriptPath`, async () => {
 		const wrapper = mount(WikibaseItem, {
 			global: { plugins: [vuetify] },
 			props: {
@@ -26,6 +26,31 @@ describe('WikibaseItem', async () => {
 		expect(title.text()).toEqual("(-1) Ahistorical Salutation Department of Figaro's")
 		const link = wiki.find('a')
 		expect(link.attributes()).toHaveProperty('href', 'https://asdf.test/script/api.php')
+		expect(link.text()).toEqual('Action API')
+	})
+
+	it(`renders properly without scriptPath`, async () => {
+		const wrapper = mount(WikibaseItem, {
+			global: { plugins: [vuetify] },
+			props: {
+				wiki: {
+					id: '-1',
+					title: "Ahistorical Salutation Department of Figaro's",
+					urls: {
+						baseUrl: 'https://asdf.test',
+						scriptPath: null
+					}
+				}
+			}
+		})
+
+		const wiki = wrapper.find('div.wikibase-item')
+		expect(wiki.exists()).toEqual(true)
+		const title = wiki.find('h4')
+		expect(title.exists()).toEqual(true)
+		expect(title.text()).toEqual("(-1) Ahistorical Salutation Department of Figaro's")
+		const link = wiki.find('a')
+		expect(link.attributes()).not.toHaveProperty('href')
 		expect(link.text()).toEqual('Action API')
 	})
 })
