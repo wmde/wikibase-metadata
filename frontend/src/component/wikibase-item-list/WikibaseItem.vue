22d80aa5815593767cf589c781903e2085c81684
ricki-jay-wmde <ricki.jay@wikimedia.de>
2026-09-18 17:15:55 +0200

    Data Fetcher

--- frontend/src/component/wikibase-item-list/WikibaseItem.vue
+++ frontend/src/component/wikibase-item-list/WikibaseItem.vue
@@ -1,57 +1,22 @@
 <script setup lang="ts">
 import type { WbItemFragment } from '@/graphql/types'
+import DataFetcher from '@/util/fetch-data'
 import getActionApiUrl from '@/util/getActionApiUrl'
-import { computed, ref, watch } from 'vue'
+import { computed, watch } from 'vue'
 
 const { searchValue, wiki } = defineProps<{ searchValue: string; wiki: WbItemFragment }>()
 
-const actionApiUrl = computed(() => getActionApiUrl(wiki.urls.baseUrl, wiki.urls.scriptPath))
+const fetcher = new DataFetcher(getActionApiUrl(wiki.urls.baseUrl, wiki.urls.scriptPath))
 
-const data = ref<SearchResult>()
-const status = ref<{ code: number; text: string }>()
-const loading = ref(false)
-const getData = async () => {
-	if (actionApiUrl.value && searchValue) {
-		const request = new Request(
-			`${actionApiUrl.value}?action=wbsearchentities&search=${searchValue}&language=en&format=json`,
-			{ headers: [] }
-		)
-		loading.value = true
-		const response = await fetch(request)
-		status.value = { code: response.status, text: response.statusText }
-		if (status.value.code == 200) {
-			data.value = (await response.json()) as SearchResult
-		}
-		loading.value = false
-	}
-}
-
-type SearchResult = {
-	searchInfo: {
-		search: string
-	}
-	search: {
-		id: string
-		title: string
-		pageid: number
-		repository: string
-		url: string
-		concepturi: string
-		label: string
-		description: string
-		match: {
-			type: string
-			language: string
-			text: string
-		}
-	}[]
-	success: number
-}
+const actionApiUrl = computed(() => fetcher.actionApiUrl)
+const data = computed(() => fetcher.data)
+const status = computed(() => fetcher.status)
+const loading = computed(() => fetcher.loading)
 
 // You cannot directly watch the prop for changes
 // But you can watch, essentially, a reference to the value of the prop
 const searchValueRef = computed(() => searchValue)
-watch(searchValueRef, getData)
+watch(searchValueRef, () => fetcher.getData(searchValueRef.value))
 </script>
 
 <template>
@@ -62,6 +27,7 @@ watch(searchValueRef, getData)
 		</p>
 		<p>Searching: {{ searchValue }}</p>
 		<p>Loading: {{ loading }}</p>
+		<p>Status: {{ status }}</p>
 		<p>Result: {{ data }}</p>
 	</div>
 </template>
