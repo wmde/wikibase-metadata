fbb9f5282f63dc31641beb9c4caa5e37497b7cdc
Ricki <141253288+RickiJay-WMDE@users.noreply.github.com>
2026-09-22 11:00:24 +0200

    Apply suggestion from @JacobThwaites
    Co-authored-by: Jacob Thwaites <43198438+JacobThwaites@users.noreply.github.com>

--- frontend/src/component/wikibase-table/__tests__/WikibaseSearch.items.spec.ts
+++ frontend/src/component/wikibase-table/__tests__/WikibaseSearch.items.spec.ts
@@ -39,20 +39,4 @@ describe('WikibaseSearch', async () => {
 		expect(menuButton.text()).toEqual('Items')
 	})
 
-	it('renders menu properly', async () => {
-		const wrapper = mount(WikibaseSearch, {
-			global: { plugins: [vuetify] },
-			props: { menuValue: 'items', setMenuValue: mockSetMenuValue }
-		})
-
-		const container = wrapper.find('.search-container')
-		expect(container.exists()).toEqual(true)
-
-		const searchContainer = container.find('.search-text')
-		expect(searchContainer.exists()).toEqual(true)
-
-		const menuButton = searchContainer.find('.v-btn')
-		expect(menuButton.exists()).toEqual(true)
-		expect(menuButton.text()).toEqual('Items')
-	})
 })
